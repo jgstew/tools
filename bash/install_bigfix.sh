8232b5466a77c1374dfc42e0012ca04dda927e8a
jgstew <james@jgstew.com>
2016-05-10 15:29:28 -0700

    Update install_bigfix.sh

--- bash/install_bigfix.sh
+++ bash/install_bigfix.sh
@@ -135,3 +135,4 @@ fi
 # - http://wiki.bash-hackers.org/scripting/posparams
 # - http://stackoverflow.com/questions/6482377/check-existence-of-input-argument-in-a-bash-shell-script
 # - http://stackoverflow.com/questions/592620/check-if-a-program-exists-from-a-bash-script
+# - https://forum.bigfix.com/t/script-to-kickstart-the-installation-of-bigfix-on-os-x-debian-family-rhel-family/17023
