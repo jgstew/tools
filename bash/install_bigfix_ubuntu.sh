ffd0775ca4336d0e2746f2de214857f83c3fbf4e
jgstew <james@jgstew.com>
2016-05-10 00:27:58 -0700

    Create install_bigfix_ubuntu.sh

---
+++ bash/install_bigfix_ubuntu.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# kickstart bigfix install
+# current target - ubuntu
+# 
+
+if [ "$(id -u)" != "0" ]; then
+	echo "Sorry, you are not root."
+	exit 1
+fi
+
+INSTALLDIR="/etc/opt/BESClient"
+MASTHEAD="http://_ROOT_OR_RELAY_SERVER_FQDN_:52311/masthead/masthead.afxm"
+INSTALLER="http://software.bigfix.com/download/bes/95/BESAgent-9.5.1.9-ubuntu10.amd64.deb"
+
+if [ ! -d "$INSTALLDIR" ]; then
+  # Control will enter here if $DIRECTORY doesn't exist.
+  mkdir $INSTALLDIR
+fi
+
+# Download the BigFix agent
+curl -o BESAgent.deb $INSTALLER
+# Download the masthead, renamed, into the correct location
+curl -o $INSTALLDIR/actionsite.afxm $MASTHEAD
+
+# install BigFix client
+dpkg -i BESAgent.deb
+
+# start the BigFix client
+/etc/init.d/besclient start
