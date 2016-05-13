9b5249484deaf0e31b346041d8dd5ff1fab8b0d8
jgstew <james@jgstew.com>
2016-05-13 00:11:45 -0700

    adding SetSecurityFlags to ignore SSL validation

--- CMD/install_bigfix.bat
+++ CMD/install_bigfix.bat
@@ -4,7 +4,8 @@ SET MASTHEADURL=http://%1:52311/masthead/masthead.afxm
 ECHO
 
 bitsadmin /transfer bigfixdl%random% /download /priority FOREGROUND http://software.bigfix.com/download/bes/95/BigFix-BES-Client-9.5.1.9.exe C:\Windows\Temp\bigfixclient.exe
-bitsadmin /transfer bigfixcfgdl%random% /download /priority FOREGROUND %MASTHEADURL% C:\Windows\Temp\masthead.afxm
+SET MASTHEADJOB=bigfixmhdl%random%
+bitsadmin /transfer %MASTHEADJOB% /SetSecurityFlags %MASTHEADJOB% 8 /download /priority FOREGROUND %MASTHEADURL% C:\Windows\Temp\masthead.afxm
 
 ECHO I am %COMSPEC%
 ECHO put CMD/BAT lines in here.
