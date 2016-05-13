fed0d344c3f5ee19ee737685d0664436035c26e9
jgstew <james@jgstew.com>
2016-05-13 00:25:09 -0700

    Update install_bigfix.bat

--- CMD/install_bigfix.bat
+++ CMD/install_bigfix.bat
@@ -3,9 +3,9 @@
 SET MASTHEADURL=http://%1:52311/masthead/masthead.afxm
 ECHO
 
-bitsadmin /transfer bigfixdl%random% /download /priority FOREGROUND http://software.bigfix.com/download/bes/95/BigFix-BES-Client-9.5.1.9.exe C:\Windows\Temp\bigfixclient.exe
+bitsadmin /transfer bigfixdl%random% /download /priority HIGH http://software.bigfix.com/download/bes/95/BigFix-BES-Client-9.5.1.9.exe C:\Windows\Temp\bigfixclient.exe
 SET MASTHEADJOB=bigfixmhdl%random%
-bitsadmin /transfer %MASTHEADJOB% /SetSecurityFlags %MASTHEADJOB% 8 /download /priority FOREGROUND %MASTHEADURL% C:\Windows\Temp\masthead.afxm
+bitsadmin /transfer %MASTHEADJOB% /SetSecurityFlags %MASTHEADJOB% 8 /download /priority HIGH %MASTHEADURL% C:\Windows\Temp\masthead.afxm
 
 ECHO I am %COMSPEC%
 ECHO put CMD/BAT lines in here.
