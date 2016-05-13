53334204354f1797bd187163ad6a6c8c1b6229a1
jgstew <james@jgstew.com>
2016-05-12 20:09:00 -0700

    Update install_bigfix.bat

--- CMD/install_bigfix.bat
+++ CMD/install_bigfix.bat
@@ -2,11 +2,15 @@
 @ECHO OFF
 SET MASTHEADURL=http://%1:52311/masthead/masthead.afxm
 ECHO
+
+bitsadmin /transfer bigfixdl%random% /download /priority FOREGROUND http://software.bigfix.com/download/bes/95/BigFix-BES-Client-9.5.1.9.exe C:\Windows\Temp\bigfixclient.exe
+
 ECHO I am %COMSPEC%
 ECHO put CMD/BAT lines in here.
 ECHO %0
 ECHO %1
 ECHO %MASTHEADURL%
+
 PAUSE
 EXIT
 ::CMDLITERAL
