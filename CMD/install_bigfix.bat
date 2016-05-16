bddab8e21a9cc58753cb64ba8296d256766bbf1c
jgstew <james@jgstew.com>
2016-05-15 18:34:46 -0700

    Update install_bigfix.bat

--- CMD/install_bigfix.bat
+++ CMD/install_bigfix.bat
@@ -7,7 +7,7 @@ REM http://stackoverflow.com/questions/4781772/how-to-test-if-an-executable-exis
 where /q powershell || ECHO Cound not find powershell. && EXIT /B
 
 powershell -command "& { (New-Object Net.WebClient).DownloadFile('http://software.bigfix.com/download/bes/95/BigFix-BES-Client-9.5.1.9.exe', 'c:\Windows\Temp\BESClient.exe') }" -ExecutionPolicy Bypass
-powershell -command "& { (New-Object Net.WebClient).DownloadFile('%MASTHEADURL%', 'c:\Windows\Temp\masthead.afxm') }" -ExecutionPolicy Bypass
+powershell -command "& { (New-Object Net.WebClient).DownloadFile('%MASTHEADURL%', 'c:\Windows\Temp\actionsite.afxm') }" -ExecutionPolicy Bypass
 
 ECHO I am %COMSPEC%
 ECHO put CMD/BAT lines in here.
