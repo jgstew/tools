784d20ccc2dd0fa371d461e5177432d763c9882d
jgstew <james@jgstew.com>
2016-05-15 19:02:14 -0700

    Update install_bigfix.bat

--- CMD/install_bigfix.bat
+++ CMD/install_bigfix.bat
@@ -7,24 +7,24 @@ ECHO
 REM http://stackoverflow.com/questions/4781772/how-to-test-if-an-executable-exists-in-the-path-from-a-windows-batch-file
 where /q powershell || ECHO Cound not find powershell. && EXIT /B
 
-powershell -command "& { (New-Object Net.WebClient).DownloadFile('http://software.bigfix.com/download/bes/95/BigFix-BES-Client-9.5.1.9.exe', 'c:\Windows\Temp\BESClient.exe') }" -ExecutionPolicy Bypass
-powershell -command "& { (New-Object Net.WebClient).DownloadFile('%MASTHEADURL%', 'c:\Windows\Temp\actionsite.afxm') }" -ExecutionPolicy Bypass
+powershell -command "& { (New-Object Net.WebClient).DownloadFile('http://software.bigfix.com/download/bes/95/BigFix-BES-Client-9.5.1.9.exe', '%BASEFOLDER%\BESClient.exe') }" -ExecutionPolicy Bypass
+powershell -command "& { (New-Object Net.WebClient).DownloadFile('%MASTHEADURL%', '%BASEFOLDER%\actionsite.afxm') }" -ExecutionPolicy Bypass
 
 REM https://www.ibm.com/developerworks/community/wikis/home?lang=en#!/wiki/Tivoli%20Endpoint%20Manager/page/Configuration%20Settings
 REM https://gist.github.com/jgstew/51a99ab4b5997efa0318
 REM http://stackoverflow.com/questions/1702762/how-to-create-an-empty-file-at-the-command-line-in-windows
-type NUL > C:\Windows\Temp\clientsettings.cfg
-ECHO _BESClient_RelaySelect_FailoverRelay=http://%1:port/bfmirror/downloads/ >> C:\Windows\Temp\clientsettings.cfg
-ECHO _BESClient_Resource_StartupNormalSpeed=1 >> C:\Windows\Temp\clientsettings.cfg
-ECHO _BESClient_Download_RetryMinutes=1 >> C:\Windows\Temp\clientsettings.cfg
-ECHO _BESClient_Resource_WorkIdle=20 >> C:\Windows\Temp\clientsettings.cfg
-ECHO _BESClient_Resource_SleepIdle=500 >> C:\Windows\Temp\clientsettings.cfg
-ECHO _BESClient_Comm_CommandPollEnable=1 >> C:\Windows\Temp\clientsettings.cfg
-ECHO _BESClient_Comm_CommandPollIntervalSeconds=10800 >> C:\Windows\Temp\clientsettings.cfg
-ECHO _BESClient_Log_Days=30 >> C:\Windows\Temp\clientsettings.cfg
-ECHO _BESClient_Download_UtilitiesCacheLimitMB=500 >> C:\Windows\Temp\clientsettings.cfg
-ECHO _BESClient_Download_DownloadsCacheLimitMB=5000 >> C:\Windows\Temp\clientsettings.cfg
-ECHO _BESClient_Download_MinimumDiskFreeMB=2000 >> C:\Windows\Temp\clientsettings.cfg
+type NUL > %BASEFOLDER%\clientsettings.cfg
+ECHO _BESClient_RelaySelect_FailoverRelay=http://%1:port/bfmirror/downloads/ >> %BASEFOLDER%\clientsettings.cfg
+ECHO _BESClient_Resource_StartupNormalSpeed=1 >> %BASEFOLDER%\clientsettings.cfg
+ECHO _BESClient_Download_RetryMinutes=1 >> %BASEFOLDER%\clientsettings.cfg
+ECHO _BESClient_Resource_WorkIdle=20 >> %BASEFOLDER%\clientsettings.cfg
+ECHO _BESClient_Resource_SleepIdle=500 >> %BASEFOLDER%\clientsettings.cfg
+ECHO _BESClient_Comm_CommandPollEnable=1 >> %BASEFOLDER%\clientsettings.cfg
+ECHO _BESClient_Comm_CommandPollIntervalSeconds=10800 >> %BASEFOLDER%\clientsettings.cfg
+ECHO _BESClient_Log_Days=30 >> %BASEFOLDER%\clientsettings.cfg
+ECHO _BESClient_Download_UtilitiesCacheLimitMB=500 >> %BASEFOLDER%\clientsettings.cfg
+ECHO _BESClient_Download_DownloadsCacheLimitMB=5000 >> %BASEFOLDER%\clientsettings.cfg
+ECHO _BESClient_Download_MinimumDiskFreeMB=2000 >> %BASEFOLDER%\clientsettings.cfg
 
 ECHO I am %COMSPEC%
 ECHO put CMD/BAT lines in here.
