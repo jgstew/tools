01299c4babebf4ebab6f2e22a13c9c19a1e5482a
jgstew <james@jgstew.com>
2016-05-15 20:03:52 -0700

    Update install_bigfix.sh
    added creation of a clientsettings.cfg file for Mac OS X

--- bash/install_bigfix.sh
+++ bash/install_bigfix.sh
@@ -49,6 +49,20 @@ if [[ $OSTYPE == darwin* ]]; then
   INSTALLERURL="http://software.bigfix.com/download/bes/95/BESAgent-9.5.1.9-BigFix_MacOSX10.7.pkg"
   INSTALLDIR="/tmp"
   INSTALLER="/tmp/BESAgent.pkg"
+  
+  # create clientsettings.cfg file
+  echo -n > $INSTALLDIR/clientsettings.cfg
+  echo _BESClient_RelaySelect_FailoverRelay=http://$1:52311/bfmirror/downloads/ >> $INSTALLDIR/clientsettings.cfg
+  echo _BESClient_Resource_StartupNormalSpeed=1 >> $INSTALLDIR/clientsettings.cfg
+  echo _BESClient_Download_RetryMinutes=1 >> $INSTALLDIR/clientsettings.cfg
+  echo _BESClient_Resource_WorkIdle=20 >> $INSTALLDIR/clientsettings.cfg
+  echo _BESClient_Resource_SleepIdle=500 >> $INSTALLDIR/clientsettings.cfg
+  echo _BESClient_Comm_CommandPollEnable=1 >> $INSTALLDIR/clientsettings.cfg
+  echo _BESClient_Comm_CommandPollIntervalSeconds=10800 >> $INSTALLDIR/clientsettings.cfg
+  echo _BESClient_Log_Days=30 >> $INSTALLDIR/clientsettings.cfg
+  echo _BESClient_Download_UtilitiesCacheLimitMB=500 >> $INSTALLDIR/clientsettings.cfg
+  echo _BESClient_Download_DownloadsCacheLimitMB=5000 >> $INSTALLDIR/clientsettings.cfg
+  echo _BESClient_Download_MinimumDiskFreeMB=2000 >> $INSTALLDIR/clientsettings.cfg
 else
   # For most Linux:
   INSTALLDIR="/etc/opt/BESClient"
