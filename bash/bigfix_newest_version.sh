476c8cd1fa5d9f3671947c138350e3d711ed795b
jgstew <james@jgstew.com>
2016-05-20 15:13:03 -0700

    Create bigfix_newest_version.sh

---
+++ bash/bigfix_newest_version.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# https://github.com/jgstew/tools/issues/9
+
+
+# http://stackoverflow.com/questions/592620/check-if-a-program-exists-from-a-bash-script
+# FUNCTION: check if command exists
+command_exists () {
+  type "$1" &> /dev/null ;
+}
+
+HTMLURL=http://support.bigfix.com/bes/release/index.html
+
+if command_exists curl ; then
+  curl -O $HTMLURL
+else
+    wget $HTMLURL
+  else
+    echo neither wget nor curl is installed.
+    echo not able to download required files.
+    echo exiting...
+    exit 2
+  fi
+fi
+
+echo `cat index.html | grep -m 16 -i -o -P -e '<td align="left">\d+\.\d+\.\d+\.\d+<\/td>' | grep -o -P -e '\d+\.\d+\.\d+\.\d+' | sort -r -V | uniq -c | grep -m 1 -i "4 " | grep -o -P -e '\d+\.\d+\.\d+\.\d+'`
