49779b46c6ec08185bc83e4c1ad399b70bd753a3
jgstew <james@jgstew.com>
2016-05-20 15:43:09 -0700

    Update bigfix_newest_version.sh

--- bash/bigfix_newest_version.sh
+++ bash/bigfix_newest_version.sh
@@ -8,24 +8,26 @@ command_exists () {
   type "$1" &> /dev/null ;
 }
 
+download_file () {
+  # https://www.mattcutts.com/blog/how-to-fetch-a-url-with-curl-or-wget-silently/
+  if command_exists curl ; then
+    curl -silent -O "$1"
+  else
+    if command_exists wget ; then
+      wget -quiet "$1"
+    else
+      echo neither wget nor curl is installed.
+      echo not able to download required files.
+      echo exiting...
+      exit 2
+    fi
+  fi
+}
+
 if [ -n "$1" ]; then
   HTMLURL=$1
 else
   HTMLURL=http://support.bigfix.com/bes/release/index.html
-fi
-
-# https://www.mattcutts.com/blog/how-to-fetch-a-url-with-curl-or-wget-silently/
-if command_exists curl ; then
-  curl -silent -O $HTMLURL
-else
-  if command_exists wget ; then
-    wget -quiet $HTMLURL
-  else
-    echo neither wget nor curl is installed.
-    echo not able to download required files.
-    echo exiting...
-    exit 2
-  fi
 fi
 
 echo `cat index.html | grep -m 16 -i -o -P -e '<td align="left">\d+\.\d+\.\d+\.\d+<\/td>' | grep -o -P -e '\d+\.\d+\.\d+\.\d+' | sort -r -V | uniq -c | grep -m 1 -i "4 " | grep -o -P -e '\d+\.\d+\.\d+\.\d+'`
