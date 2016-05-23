c6b09f1d73cb0cad419244b7223de41c4c2272bc
jgstew <james@jgstew.com>
2016-05-23 10:11:28 -0700

    Update now.sh

--- bash/now.sh
+++ bash/now.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-# this is equivalent to bigfix relevance `now as string`
 # Reference: http://www.cyberciti.biz/faq/linux-unix-formatting-dates-for-display/
+# this is equivalent to bigfix relevance `now as string`
 date +"%a, %d %b %Y %T %z"
