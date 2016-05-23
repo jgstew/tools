9cf35b6dad99c4d5a5498c075a8ab70b361771f0
jgstew <james@jgstew.com>
2016-05-23 10:37:07 -0700

    Update download_file.sh
    made this work with 1 or 2 arguments. First argument is URL, 2nd is location to save the resulting file

--- bash/download_file.sh
+++ bash/download_file.sh
@@ -10,10 +10,18 @@ command_exists () {
 
   # https://www.mattcutts.com/blog/how-to-fetch-a-url-with-curl-or-wget-silently/
   if command_exists curl ; then
-    curl -silent -o $2 "$1"
+    if [ -n "$2" ]; then
+      curl -silent -o "$2" "$1"
+    else
+      curl -silent "$1"
+    fi
   else
     if command_exists wget ; then
-      wget "$1" -quiet -O $2
+      if [ -n "$2" ]; then
+        wget "$1" -quiet -O "$2"
+      else
+        wget "$1" -quiet
+      fi
     else
       echo neither wget nor curl is installed.
       echo not able to download required files.
