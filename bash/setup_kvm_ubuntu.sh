34ac9df6669d1d41ccbfb5aa6c10b40313bff403
jgstew <james@jgstew.com>
2016-06-09 11:01:39 -0700

    Create setup_kvm_ubuntu.sh

---
+++ bash/setup_kvm_ubuntu.sh
@@ -0,0 +1,6 @@
+
+# just a record of the commands i'm using to setup kvm on ubuntu to have many small linux vms
+
+# http://askubuntu.com/questions/448358/automating-apt-get-install-with-assume-yes
+sudo apt-get install kvm --assume-yes
+
