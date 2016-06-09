770ccaa62d61433f7261bf15ee8e1ef19977a385
jgstew <james@jgstew.com>
2016-06-09 15:10:27 -0700

    Update setup_kvm_ubuntu.sh

--- bash/setup_kvm_ubuntu.sh
+++ bash/setup_kvm_ubuntu.sh
@@ -1,6 +1,7 @@
 
 # just a record of the commands i'm using to setup kvm on ubuntu to have many small linux vms
 
+# http://xmodulo.com/use-kvm-command-line-debian-ubuntu.html
 sudo apt-get update
 # http://askubuntu.com/questions/448358/automating-apt-get-install-with-assume-yes
 sudo apt-get install kvm --assume-yes
