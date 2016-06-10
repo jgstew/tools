1e6e3773068f187898feb0dcd292ee522311a3c6
jgstew <james@jgstew.com>
2016-06-10 11:40:34 -0700

    Update and rename setup_kvm_ubuntu.sh to setup_qemu_ubuntu.sh

--- bash/setup_qemu_ubuntu.sh
+++ bash/setup_qemu_ubuntu.sh
@@ -4,10 +4,11 @@
 # http://xmodulo.com/use-kvm-command-line-debian-ubuntu.html
 sudo apt-get update
 # http://askubuntu.com/questions/448358/automating-apt-get-install-with-assume-yes
-sudo apt-get install kvm qemu-kvm libvirt-bin --assume-yes
+sudo apt-get install kvm qemu-kvm libvirt-bin qemu --assume-yes
 sudo apt-get upgrade --assume-yes
-# sudo virsh --connect qemu:///system
-
 
 # http://raspberrypi.stackexchange.com/questions/4296/can-i-emulate-x86-cpu-to-run-teamspeak-3-server
 # http://bochs.sourceforge.net/
+
+qemu-img create -f qcow2 debian.img 1500M
+qemu-system-i386 -cdrom debian-live.iso -m 400 -smp 1 -redir udp:52311::52311 debian.img
