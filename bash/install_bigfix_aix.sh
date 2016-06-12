8829d54b0f46f0c3e0f060739dd3f3167c01a0c3
jgstew <james@jgstew.com>
2016-06-12 12:02:55 -0700

    Update install_bigfix_aix.sh

--- bash/install_bigfix_aix.sh
+++ bash/install_bigfix_aix.sh
@@ -2,8 +2,11 @@
 # kickstart bigfix install
 
 # AIX 7.2 does seem to have bash in addition to sh, though sh is the default
+
 # wget is preinstalled, curl is not (normal)
 # having trouble downloading the script with wget :(
+#  whenever downloading over https, wget gives the error messagine `- : Unsupported scheme.`
+#    this is probably an HTTPS issue
 
 # `uname` = AIX
 # `uname -p` = powerpc
