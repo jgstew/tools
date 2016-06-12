5a23ef8417599e4d777b3c4849d7bdd6e90fc920
jgstew <james@jgstew.com>
2016-06-12 11:50:43 -0700

    Update install_bigfix_aix.sh
    add some comments and things

--- bash/install_bigfix_aix.sh
+++ bash/install_bigfix_aix.sh
@@ -4,3 +4,12 @@
 # AIX 7.2 does seem to have bash in addition to sh, though sh is the default
 # wget is preinstalled, curl is not (normal)
 # having trouble downloading the script with wget :(
+
+# `uname` = AIX
+# `uname -p` = powerpc
+# OSTYPE & HOSTTYPE don't work in SH on AIX
+
+command_exists () {
+  type "$1" &> /dev/null ;
+}
+
