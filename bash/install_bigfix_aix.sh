23e9ebb1a9be14ae452bcc6c63779f78aedd6f01
jgstew <james@jgstew.com>
2016-06-13 19:32:39 -0700

    removed references to wget
    wget won't work to download the kickstart file, which is annoying. python or similar is a better alternative it seems.

--- bash/install_bigfix_aix.sh
+++ bash/install_bigfix_aix.sh
@@ -3,15 +3,6 @@
 
 # AIX 7.2 does seem to have bash in addition to sh, though sh is the default
 
-# wget is preinstalled, curl is not (normal)
-# having trouble downloading the script with wget :(
-#  whenever downloading over https, wget gives the error messagine `- : Unsupported scheme.`
-#    this is probably an HTTPS issue, need to try `--no-check-certificate` (Didn't work)
-#    the `--no-check-certificate` option is not available for the wget that ships with AIX 7.2
-#
-#  http://www.bullfreeware.com/affichage.php?id=2401
-#  http://www.bullfreeware.com/download/bin/2401/wget-1.17.1-1.aix6.1.ppc.rpm
-
 # python -c "import urllib; print urllib.urlopen('https://raw.githubusercontent.com/jgstew/tools/master/bash/install_bigfix_aix.sh').read()" > install_bigfix_aix.sh
 
 # `uname` = AIX
