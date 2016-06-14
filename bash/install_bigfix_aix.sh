9c67b590301fefbf4bd876751b14c7ad8234fa2c
jgstew <james@jgstew.com>
2016-06-13 19:11:22 -0700

    Update install_bigfix_aix.sh

--- bash/install_bigfix_aix.sh
+++ bash/install_bigfix_aix.sh
@@ -6,7 +6,11 @@
 # wget is preinstalled, curl is not (normal)
 # having trouble downloading the script with wget :(
 #  whenever downloading over https, wget gives the error messagine `- : Unsupported scheme.`
-#    this is probably an HTTPS issue, need to try `--no-check-certificate`
+#    this is probably an HTTPS issue, need to try `--no-check-certificate` (Didn't work)
+#    the `--no-check-certificate` option is not available for the wget that ships with AIX 7.2
+#
+#  http://www.bullfreeware.com/affichage.php?id=2401
+#  http://www.bullfreeware.com/download/bin/2401/wget-1.17.1-1.aix6.1.ppc.rpm
 
 # `uname` = AIX
 # `uname -p` = powerpc
