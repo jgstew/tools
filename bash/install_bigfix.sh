260a471c52700dbd726d5a8d79874901f31ef731
jgstew <james@jgstew.com>
2016-08-16 20:04:05 -0700

    Update install_bigfix.sh

--- bash/install_bigfix.sh
+++ bash/install_bigfix.sh
@@ -75,7 +75,7 @@ if [ ! -f $INSTALLDIR/clientsettings.cfg ] ; then
   >> $INSTALLDIR/clientsettings.cfg echo _BESClient_Resource_WorkIdle=20
   >> $INSTALLDIR/clientsettings.cfg echo _BESClient_Resource_SleepIdle=500
   >> $INSTALLDIR/clientsettings.cfg echo _BESClient_Comm_CommandPollEnable=1
-  >> $INSTALLDIR/clientsettings.cfg echo _BESClient_Comm_CommandPollIntervalSeconds=10800
+  >> $INSTALLDIR/clientsettings.cfg echo _BESClient_Comm_CommandPollIntervalSeconds=3600
   >> $INSTALLDIR/clientsettings.cfg echo _BESClient_Log_Days=30
   >> $INSTALLDIR/clientsettings.cfg echo _BESClient_Download_UtilitiesCacheLimitMB=500
   >> $INSTALLDIR/clientsettings.cfg echo _BESClient_Download_DownloadsCacheLimitMB=5000
