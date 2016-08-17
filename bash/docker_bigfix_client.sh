dd4a835141bf23246cdc5e740e0311240e4637bc
jgstew <james@jgstew.com>
2016-08-16 23:16:37 -0700

    Update docker_bigfix_client.sh
    removed the `-it` that shouldn't be there

--- bash/docker_bigfix_client.sh
+++ bash/docker_bigfix_client.sh
@@ -5,7 +5,7 @@
 # replace `alpha.bigfix.com` with your own BigFix root server or relay
 sudo docker run -d ubuntu bash -c "apt-get update;apt-get install wget -y;wget https://raw.githubusercontent.com/jgstew/tools/master/bash/install_bigfix.sh;chmod u+x install_bigfix.sh;./install_bigfix.sh alpha.bigfix.com;tail -f /dev/null"
 
-sudo docker run -it centos bash -c "yum install initscripts -y;cd /tmp;curl -O https://raw.githubusercontent.com/jgstew/tools/master/bash/install_bigfix.sh;chmod u+x install_bigfix.sh;./install_bigfix.sh alpha.bigfix.com;tail -f /dev/null"
+sudo docker run -d centos bash -c "yum install initscripts -y;cd /tmp;curl -O https://raw.githubusercontent.com/jgstew/tools/master/bash/install_bigfix.sh;chmod u+x install_bigfix.sh;./install_bigfix.sh alpha.bigfix.com;tail -f /dev/null"
 
 # http://stackoverflow.com/questions/30209776/docker-container-will-automatically-stop-after-docker-run-d
 # http://stackoverflow.com/a/33497697/861745
