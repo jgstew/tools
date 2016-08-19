4ec762bd6349c6935b48b19adad010154cf5f3b7
jgstew <james@jgstew.com>
2016-08-18 18:25:42 -0700

    Update docker_bigfix_client.sh

--- bash/docker_bigfix_client.sh
+++ bash/docker_bigfix_client.sh
@@ -7,6 +7,7 @@
 sudo docker run -d ubuntu bash -c "apt-get update;apt-get install wget -y;wget https://raw.githubusercontent.com/jgstew/tools/master/bash/install_bigfix.sh;chmod u+x install_bigfix.sh;./install_bigfix.sh alpha.bigfix.com;tail -f /dev/null"
 
 sudo docker run -d centos bash -c "yum install initscripts -y;cd /tmp;curl -O https://raw.githubusercontent.com/jgstew/tools/master/bash/install_bigfix.sh;chmod u+x install_bigfix.sh;./install_bigfix.sh alpha.bigfix.com;tail -f /dev/null"
+sudo docker run -d fedora bash -c "yum install initscripts -y;cd /tmp;curl -O https://raw.githubusercontent.com/jgstew/tools/master/bash/install_bigfix.sh;chmod u+x install_bigfix.sh;./install_bigfix.sh alpha.bigfix.com;tail -f /dev/null"
 
 # http://stackoverflow.com/questions/30209776/docker-container-will-automatically-stop-after-docker-run-d
 # http://stackoverflow.com/a/33497697/861745
