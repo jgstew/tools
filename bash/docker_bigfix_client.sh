2d6d4d6794a8decb3c47b3799941d3a7c104e340
jgstew <james@jgstew.com>
2016-08-18 17:33:37 -0700

    Update docker_bigfix_client.sh

--- bash/docker_bigfix_client.sh
+++ bash/docker_bigfix_client.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #
 # This script, when run on a host with Docker installed, will run an ubuntu/centos container and install the BigFix client
+#      Related: https://github.com/bigfix/bfdocker/tree/master/besclient
 #
 # replace `alpha.bigfix.com` with your own BigFix root server or relay
 sudo docker run -d ubuntu bash -c "apt-get update;apt-get install wget -y;wget https://raw.githubusercontent.com/jgstew/tools/master/bash/install_bigfix.sh;chmod u+x install_bigfix.sh;./install_bigfix.sh alpha.bigfix.com;tail -f /dev/null"
