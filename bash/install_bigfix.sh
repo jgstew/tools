ed84f79e4ab822f4a71ae5babc48e8ac86f115d0
jgstew <james@jgstew.com>
2016-09-16 13:24:19 -0700

    Update install_bigfix.sh
    $SUDO_USER seems to not be working correctly.

--- bash/install_bigfix.sh
+++ bash/install_bigfix.sh
@@ -81,6 +81,7 @@ if [ ! -f $INSTALLDIR/clientsettings.cfg ] ; then
   >> $INSTALLDIR/clientsettings.cfg echo _BESClient_Download_UtilitiesCacheLimitMB=500
   >> $INSTALLDIR/clientsettings.cfg echo _BESClient_Download_DownloadsCacheLimitMB=5000
   >> $INSTALLDIR/clientsettings.cfg echo _BESClient_Download_MinimumDiskFreeMB=2000
+  # TODO: the following line needs tested. Seems to not be working in docker containers, or perhaps not at all.
   >> $INSTALLDIR/clientsettings.cfg echo _BESClient_InstallTime_User=$SUDO_USER
 fi
 
