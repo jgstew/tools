0579a0318adc2b7beafa9e77c4f383c94802ffaa
jgstew <james@jgstew.com>
2016-12-19 14:34:14 -0800

    Create edit_ini.py
    tested successfully on OS X using the built in python.

---
+++ Python/edit_ini.py
@@ -0,0 +1,9 @@
+
+# http://stackoverflow.com/questions/11637467/modify-ini-file-with-python/11637494#11637494
+
+import ConfigParser
+config=ConfigParser.RawConfigParser()
+config.read(r'/tmp/gui.cfg')
+config.set('gui','update_success_tray_report_enabled',r'yes')
+with open(r'/tmp/gui.cfg', 'wb') as configfile:
+    config.write(configfile)
