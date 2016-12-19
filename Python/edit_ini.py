fdc7ad160fea33f4f8b4afe650489ca122d37eac
jgstew <james@jgstew.com>
2016-12-19 14:51:42 -0800

    Update edit_ini.py
    add comment about https://docs.python.org/3/reference/lexical_analysis.html#string-literals

--- Python/edit_ini.py
+++ Python/edit_ini.py
@@ -1,6 +1,7 @@
 
 # http://stackoverflow.com/questions/11637467/modify-ini-file-with-python/11637494#11637494
 # https://bigfix.me/relevance/details/3019050#comments
+# http://stackoverflow.com/questions/2081640/what-exactly-do-u-and-r-string-flags-do-in-python-and-what-are-raw-string-l
 
 import ConfigParser
 config=ConfigParser.RawConfigParser()
