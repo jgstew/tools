f312e226f879958bf20e1a8a402603db1109a4ee
jgstew <james@jgstew.com>
2017-01-13 10:39:52 -0800

    Update GetFunctions.js

--- JS/GetFunctions.js
+++ JS/GetFunctions.js
@@ -1,5 +1,6 @@
 
 // http://stackoverflow.com/questions/11279441/return-all-of-the-functions-that-are-defined-in-a-javascript-file
+// http://www.w3schools.com/js/js_function_definition.asp
 
 // `this` contains all objects/functions/etc...
 // if( this[i] ) checks for existance
@@ -8,10 +9,13 @@
 // ~this[i].toString().indexOf("[native code]") this returns 0 (false) if `[native code]` is not within the declaration
 // if( !~this[i].toString().indexOf("[native code]") ) checks that `[native code]` is not within the declaration
 
-document.write('<h2>Javascript Functions:</h2><ul>');
-for(var i in this) {
-    if( this[i] && "function"==(typeof this[i]).toString() && !~this[i].toString().indexOf("[native code]") ) {
-        document.write('<li>'+ this[i].toString() +"</li>");
+// self-invoking function expression:
+(function () {
+    document.write('<h2>Javascript Functions:</h2><ul>');
+    for(var i in this) {
+        if( this[i] && "function"==(typeof this[i]).toString() && !~this[i].toString().indexOf("[native code]") ) {
+            document.write('<li>'+ this[i].toString() +"</li>");
+        }
     }
-}
-document.write('</ul>');
+    document.write('</ul>');
+})();
