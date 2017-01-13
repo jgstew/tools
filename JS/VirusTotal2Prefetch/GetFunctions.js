087d84d6f15694b1041a37540b78732166e7a987
jgstew <james@jgstew.com>
2017-01-13 10:27:30 -0800

    Update GetFunctions.js

--- JS/VirusTotal2Prefetch/GetFunctions.js
+++ JS/VirusTotal2Prefetch/GetFunctions.js
@@ -1,8 +1,15 @@
 
 // http://stackoverflow.com/questions/11279441/return-all-of-the-functions-that-are-defined-in-a-javascript-file
 
+// `this` contains all objects/functions/etc...
+// if( this[i] ) checks for existance
+// if( "function"==(typeof this[i]).toString() ) checks that it is a function
+// this[i].toString() returns the function declaration
+// ~this[i].toString().indexOf("[native code]") this returns 0 (false) if `[native code]` is not within the declaration
+// if( !~this[i].toString().indexOf("[native code]") ) checks that `[native code]` is not within the declaration
+
 for(var i in this) {
-    if( this[i] && "function"==(typeof this[i]).toString() ) {
-        document.write('<li>'+ (this[i]).toString() +"</li>");
+    if( this[i] && "function"==(typeof this[i]).toString() && !~this[i].toString().indexOf("[native code]") ) {
+        document.write('<li>'+ this[i].toString() +"</li>");
     }
 }
