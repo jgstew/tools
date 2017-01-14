cd91ce0b2a4105aa8cdc98fad5f9058a6cdb958d
jgstew <james@jgstew.com>
2017-01-13 16:47:58 -0800

    Create removescript.js
    this doesn't seem to work yet.

---
+++ JS/removescript.js
@@ -0,0 +1,12 @@
+// http://stackoverflow.com/questions/4755546/remove-div-tag-using-javascript-or-jquery
+//  - http://stackoverflow.com/questions/4726362/use-javascript-to-prevent-a-later-script-tag-from-being-evaluated
+
+// http://www.w3schools.com/jsref/met_document_getelementsbytagname.asp
+var scripts = document.getElementsByTagName('script');
+
+// http://www.w3schools.com/js/js_loop_for.asp
+for(i = 0; i < scripts.length; i++) {
+    if (scripts[i].hasAttribute('src') && scripts[i].getAttribute('src') == 'C:\Program Files (x86)\BigFix Enterprise\BES Console\reference\wizards.js') {
+         scripts[i].parentNode.removeChild(scripts[i]);
+    }
+}
