c2b3000122f634d82bd0803c2043e9a1f69d13bc
jgstew <james@jgstew.com>
2017-01-13 16:53:47 -0800

    Update removescript.js

--- JS/removescript.js
+++ JS/removescript.js
@@ -1,3 +1,5 @@
+// NOTE: this isn't working for me... intended to prevent loading of `wizard.js`
+
 // http://stackoverflow.com/questions/4755546/remove-div-tag-using-javascript-or-jquery
 //  - http://stackoverflow.com/questions/4726362/use-javascript-to-prevent-a-later-script-tag-from-being-evaluated
 
