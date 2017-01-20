8363e872d4fec4d1996688c06b0318c3287cc600
jgstew <james@jgstew.com>
2017-01-19 17:43:45 -0800

    Create GetChartDataNVD3.js

---
+++ JS/GetChartDataNVD3.js
@@ -0,0 +1,20 @@
+
+// from here:  https://github.com/jgstew/bigfix-content/blob/master/dashboards/BaselineStatusResults.ojo
+
+// Helper function designed to take jquery array result as input from a table and put it in the format NVD3 expects
+//  - example values:  $("#results > tr > td:nth-child(2)")
+//  - example lables:  $("#results > tr > td:nth-child(4)")
+//  - usage: getChartData( $("#results > tr > td:nth-child(2)") , $("#results > tr > td:nth-child(4)") );
+function getChartData(values, labels) {
+    var arrKeyValuePairs = [];
+    for (i = 0; i < values.length; i++) { 
+        arrKeyValuePairs[i] = {
+            "label": labels[i].innerText,
+            "value": values[i].innerText
+        };
+    }
+    return arrKeyValuePairs;
+}
+
+// http://stackoverflow.com/questions/8375625/how-to-select-a-table-column-with-jquery
+// http://stackoverflow.com/questions/1168807/how-can-i-add-a-key-value-pair-to-a-javascript-object
