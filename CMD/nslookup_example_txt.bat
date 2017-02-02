c2b2a00ce733fc95652a45fe2d26233128684756
jgstew <james@jgstew.com>
2017-02-01 16:05:51 -0800

    Create nslookup_example_txt.bat
    nslookup example

---
+++ CMD/nslookup_example_txt.bat
@@ -0,0 +1,7 @@
+
+REM query the DNS `TXT` record of `google.com` with a 15 second time out
+nslookup -q=TXT -timeout=15 google.com
+
+REM https://blogs.technet.microsoft.com/rmilne/2015/09/11/how-to-use-nslookup-to-check-dns-txt-record/
+REM http://www.windowscommandline.com/nslookup/
+REM https://www.owasp.org/index.php/OWASP_File_Hash_Repository
