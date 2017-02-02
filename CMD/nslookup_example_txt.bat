b6140e5df1074cef598be517e5c80ad444cd816c
jgstew <james@jgstew.com>
2017-02-01 16:09:38 -0800

    Update nslookup_example_txt.bat
    added links

--- CMD/nslookup_example_txt.bat
+++ CMD/nslookup_example_txt.bat
@@ -5,3 +5,5 @@ nslookup -q=TXT -timeout=15 google.com
 REM https://blogs.technet.microsoft.com/rmilne/2015/09/11/how-to-use-nslookup-to-check-dns-txt-record/
 REM http://www.windowscommandline.com/nslookup/
 REM https://www.owasp.org/index.php/OWASP_File_Hash_Repository
+REM http://www.ghacks.net/2010/02/16/whitelist-hash-database-frontend/
+REM https://github.com/rjhansen/nsrllookup
