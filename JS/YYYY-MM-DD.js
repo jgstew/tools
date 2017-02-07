61a0bbe87ab8ef7559508c76a5807d51658765ba
jgstew <james@jgstew.com>
2017-02-07 11:02:01 -0800

    Update YYYY-MM-DD.js
    changed to a function

--- JS/YYYY-MM-DD.js
+++ JS/YYYY-MM-DD.js
@@ -1,2 +1,13 @@
+
 // http://stackoverflow.com/a/16714931/861745
-(new Date()).toISOString().slice(0,10)
+function fnYYYY_MM_DD(objDate) {
+	if (objDate === undefined) {
+		objDate = new Date();
+	}
+	return objDate.toISOString().slice(0,10);
+}
+
+// if this file is run directly, do the following:
+if (require.main === module) {
+	console.log( fnYYYY_MM_DD() );
+}
