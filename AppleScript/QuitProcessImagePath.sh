2f30d405e54ea8c477e1c62f5f2cab6c294872e6
jgstew <james@jgstew.com>
2017-02-13 22:27:45 -0800

    Update QuitProcessImagePath.sh
    added references

--- AppleScript/QuitProcessImagePath.sh
+++ AppleScript/QuitProcessImagePath.sh
@@ -1,1 +1,4 @@
 osascript -e 'quit application "/Applications/Minecraft.app/Contents/runtime/jre-x64/1.8.0_74/bin/java"'
+
+# - http://osxdaily.com/2014/09/05/gracefully-quit-application-command-line/
+# - https://bigfix.me/relevance/details/3019410
