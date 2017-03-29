436a201bc27e54153a03d2edc6d5e1aec9a4bb2d
jgstew <james@jgstew.com>
2017-03-28 19:34:06 -0700

    Update YYYY-MM-DD.js

--- JS/YYYY-MM-DD.js
+++ JS/YYYY-MM-DD.js
@@ -8,6 +8,7 @@ function fnYYYY_MM_DD(objDate) {
 }
 
 // if this file is run directly, do the following:
-if (require.main === module) {
+if (typeof require == "undefined" || require.main === module) {
+	// do the following only if file run directly with node or if it is run in the browser. 
 	console.log( fnYYYY_MM_DD() );
 }
