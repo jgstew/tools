a49664d9d71b34a83270c1933bcc2bbcbf57434c
jgstew <james@jgstew.com>
2017-03-30 14:37:11 -0700

    Update GetMinsBetweenDates.js
    made returned value the absolute difference. Made 2nd date parameter optional.

--- JS/GetMinsBetweenDates.js
+++ JS/GetMinsBetweenDates.js
@@ -1,6 +1,9 @@
 
 // http://www.htmlgoodies.com/html5/javascript/calculating-the-difference-between-two-dates-in-javascript.html
 Date.minsBetween = function( date1, date2 ) {
+  if(!date2){
+	date2 = new Date();
+  }
   //Get 1 min in milliseconds
   var one_min=1000*60;
 
@@ -9,7 +12,7 @@ Date.minsBetween = function( date1, date2 ) {
   var date2_ms = new Date(date2).getTime();
 
   // Calculate the difference in milliseconds
-  var difference_ms = date2_ms - date1_ms;
+  var difference_ms = Math.abs(date2_ms - date1_ms);
     
   // Convert back to min and return
   return Math.round(difference_ms/one_min); 
