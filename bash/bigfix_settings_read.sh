2e6593db64c514d64a9fd1a5d143af1c0b8d6528
jgstew <james@jgstew.com>
2017-07-13 14:38:05 -0400

    Update bigfix_settings_read.sh

--- bash/bigfix_settings_read.sh
+++ bash/bigfix_settings_read.sh
@@ -1,5 +1,9 @@
 
 # OS X
+# http://fgimian.github.io/blog/2015/06/27/a-simple-plistbuddy-tutorial/
+# http://blog.eexit.net/osx-dealing-with-property-list-files/
+# https://developer.apple.com/legacy/library/documentation/Darwin/Reference/ManPages/man8/PlistBuddy.8.html
+# https://www.ibm.com/developerworks/community/wikis/home?lang=en#!/wiki/Tivoli+Endpoint+Manager/page/Common+File+Locations
 /usr/libexec/plistbuddy -c "Print :Settings:Client" /Library/Preferences/com.bigfix.BESAgent.plist
 
 # Linux/Unix ? TODO
