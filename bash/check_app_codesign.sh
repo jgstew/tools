dd192eabe289648f02440577a14d3181c3457f74
jgstew <james@jgstew.com>
2017-07-13 10:12:08 -0400

    Update check_app_codesign.sh

--- bash/check_app_codesign.sh
+++ bash/check_app_codesign.sh
@@ -1,7 +1,7 @@
 
 # Check signatures of /Applications
 # https://stackoverflow.com/a/20292636
-cd /Applications && find . -maxdepth 1 -type d \( ! -name . \) -exec sh -c 'spctl --assess "{}"' \;
+cd /Applications && find . -iname \*.app -maxdepth 3 -type d \( ! -name . \) -exec sh -c 'spctl --assess "{}"' \;
 
 # https://macadmins.slack.com/archives/C066MCBT2/p1499952935591722
 # From elliot: find /Applications -iname *.app -maxdepth 3 -exec spctl --assess "{}" ';'
