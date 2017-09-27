fba12824a691a974ba90a0044ae53b5a6af05fce
jgstew <james@jgstew.com>
2017-09-26 19:13:11 -0700

    Create bigfixlogs.sh

---
+++ bash/bigfixlogs.sh
@@ -0,0 +1,12 @@
+
+if [ -d "/Library/Application Support/Bigfix/BES Agent/__BESData/__Global/Logs" ]; then
+  LogFolder="/Library/Application Support/Bigfix/BES Agent/__BESData/__Global/Logs"
+else
+  LogFolder="/var/opt/BESClient/__BESData/__Global/Logs"
+fi
+
+ls $LogFolder
+
+# /var/opt/BESClient/__BESData/__Global/Logs
+# /Library/Application Support/Bigfix/BES Agent/__BESData/__Global/Logs
+# https://www.ibm.com/developerworks/community/wikis/home?lang=en#!/wiki/Tivoli+Endpoint+Manager/page/Common+File+Locations
