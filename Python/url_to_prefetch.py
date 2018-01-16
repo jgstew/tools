24d4062f879d12f89f1a40e304cddefeeb450320
jgstew <james@jgstew.com>
2018-01-16 10:42:04 -0800

    Update url_to_prefetch.py

--- Python/url_to_prefetch.py
+++ Python/url_to_prefetch.py
@@ -5,6 +5,10 @@
 
 # NOTE: not sure if `size` is always accurate. Need to investigate further with more examples and different `chunksize`. TODO
 
+##  Example Results:
+#  Input: http://download.windowsupdate.com/d/msdownload/update/software/secu/2016/07/windows10.0-kb3172729-x64_18df742fad6bebc01e617c2d4f92e0d325e5138f.msu
+# Output: prefetch testfile sha1:18df742fad6bebc01e617c2d4f92e0d325e5138f size:199259 http://download.windowsupdate.com/d/msdownload/update/software/secu/2016/07/windows10.0-kb3172729-x64_18df742fad6bebc01e617c2d4f92e0d325e5138f.msu sha256:f5b55d436056a905e755984d457bac67295ad3e11531a6c33f3812cfb63ce010
+
 from __future__ import with_statement
 from hashlib import sha1, sha256
 
