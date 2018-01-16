3c8fdfa68a5cbf98b8c353823787a7db1f5f0b0d
jgstew <james@jgstew.com>
2018-01-15 17:21:57 -0800

    Update url_to_prefetch.py

--- Python/url_to_prefetch.py
+++ Python/url_to_prefetch.py
@@ -1,6 +1,11 @@
 from __future__ import with_statement
 from hashlib import sha1, sha256
 
+try:
+    from urllib.request import urlopen # Python 3
+except ImportError:
+    from urllib2 import urlopen # Python 2
+
 def url_to_prefetch(url)
   hashes = sha1(), sha256()
   chunksize = max(4096, max(h.block_size for h in hashes))
@@ -13,5 +18,6 @@ if __name__ == '__main__':
 
 # References: 
 #  - https://stackoverflow.com/questions/537542/how-can-i-create-multiple-hashes-of-a-file-using-only-one-pass
+#  - https://stackoverflow.com/questions/1517616/stream-large-binary-files-with-urllib2-to-file
 #  - https://stackoverflow.com/questions/16694907/how-to-download-large-file-in-python-with-requests-py
 #  - https://gist.github.com/Zireael-N/ed36997fd1a967d78cb2
