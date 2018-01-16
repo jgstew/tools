8f9f2de9f9f2c33b2846e92d8e95aac99d078334
jgstew <james@jgstew.com>
2018-01-16 11:09:16 -0800

    url_to_prefetch.py removed import with_statement
    only needed for python 2.5 which is rare, and only needed if using the with statement, which this code is no longer using

--- Python/url_to_prefetch.py
+++ Python/url_to_prefetch.py
@@ -12,7 +12,6 @@
 # TODO: Consider adding options to cache the file downloads & log/cache the prefetches generated
 
 
-from __future__ import with_statement # adds `with` capabilities to python 2.5 for compatibility... only need if using python 2.5 & with
 from hashlib import sha1, sha256
 
 try:
