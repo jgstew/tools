39c0c61f14e7cc04a4cc952f4bfd5e6f0cc9b392
jgstew <james@jgstew.com>
2018-01-15 16:54:09 -0800

    Create url_to_prefetch.py

---
+++ Python/url_to_prefetch.py
@@ -0,0 +1,5 @@
+
+# References: 
+#  - https://stackoverflow.com/questions/537542/how-can-i-create-multiple-hashes-of-a-file-using-only-one-pass
+#  - https://stackoverflow.com/questions/16694907/how-to-download-large-file-in-python-with-requests-py
+#  - https://gist.github.com/Zireael-N/ed36997fd1a967d78cb2
