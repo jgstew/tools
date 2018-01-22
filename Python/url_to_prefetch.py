fc56455a4f5de5ee26590df8d1c6108949f49597
jgstew <james@jgstew.com>
2018-01-22 11:18:40 -0800

    Update url_to_prefetch.py

--- Python/url_to_prefetch.py
+++ Python/url_to_prefetch.py
@@ -6,6 +6,7 @@
 # NOTE: not sure if `size` is always accurate. Need to investigate further with more examples and different `chunksize`. TODO
 
 ##  Example Results:
+# Docker: docker run python:2 bash -c "wget https://raw.githubusercontent.com/jgstew/tools/master/Python/url_to_prefetch.py ;python url_to_prefetch.py"
 #  Input: http://download.windowsupdate.com/d/msdownload/update/software/secu/2016/07/windows10.0-kb3172729-x64_18df742fad6bebc01e617c2d4f92e0d325e5138f.msu
 # Output: prefetch testfile sha1:18df742fad6bebc01e617c2d4f92e0d325e5138f size:199259 http://download.windowsupdate.com/d/msdownload/update/software/secu/2016/07/windows10.0-kb3172729-x64_18df742fad6bebc01e617c2d4f92e0d325e5138f.msu sha256:f5b55d436056a905e755984d457bac67295ad3e11531a6c33f3812cfb63ce010
 
