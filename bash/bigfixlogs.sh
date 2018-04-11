e7d66e113285dddefc2507cf619fa02c33e2e1f2
jgstew <james@jgstew.com>
2018-04-10 23:49:33 -0700

    Update bigfixlogs.sh
    fixes for MacOS (looses the verbose output of the log file being read, which could be added back manually)

--- bash/bigfixlogs.sh
+++ bash/bigfixlogs.sh
@@ -11,12 +11,12 @@ LogFile="$LogFolder/`date +%Y%m%d`.log"
 
 # Start of output:
 echo "-------------------------------------------------------------------------------------"
-echo $LogFolder
-ls $LogFolder
+echo "$LogFolder"
+ls "$LogFolder"
 echo "-------------------------------------------------------------------------------------"
 # https://stackoverflow.com/questions/36989457/retrieve-last-100-lines-logs
 # https://ss64.com/bash/tail.html
-tail --lines=${1:-25} --verbose "$LogFile"
+tail -n ${1:-25} "$LogFile"
 
 # /var/opt/BESClient/__BESData/__Global/Logs
 # /Library/Application Support/Bigfix/BES Agent/__BESData/__Global/Logs
