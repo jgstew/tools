7a1bb1c6979dbbd2a7aa14901e45f27434c83a45
jgstew <james@jgstew.com>
2018-04-17 17:49:04 -0700

    Update bigfix_settings_write.sh

--- bash/bigfix_settings_write.sh
+++ bash/bigfix_settings_write.sh
@@ -1,7 +1,7 @@
 
 # OS X
 # testing:
-echo 'sudo /usr/libexec/plistbuddy -c "Set :Settings:Client:$1:Value $2" /Library/Preferences/com.bigfix.BESAgent.plist'
+echo "sudo /usr/libexec/plistbuddy -c \"Set :Settings:Client:$1:Value $2\" /Library/Preferences/com.bigfix.BESAgent.plist"
 sudo /usr/libexec/plistbuddy -c "Print :Settings:Client:$1:Value" /Library/Preferences/com.bigfix.BESAgent.plist
 
 # Linux/Unix ? TODO
