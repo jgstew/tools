12db211dbae5b147b9248a5da281c959dd9c04bd
jgstew <james@jgstew.com>
2018-04-18 11:48:50 -0700

    Update bigfix_settings_write.sh
    add error checking

--- bash/bigfix_settings_write.sh
+++ bash/bigfix_settings_write.sh
@@ -1,6 +1,18 @@
 
+# Make sure values are provided:
+if [ -n "$1" ]; then
+  if [ -n "$2" ]; then
+    echo Setting $1 to $2
+  else
+    echo Must Provide Value
+    exit 2
+  fi
+else
+  echo Must Provide Setting
+  exit 1
+fi
+
 # OS X
-# TODO: make sure values are given
 sudo /usr/libexec/plistbuddy -c "Set :Settings:Client:$1:Value $2" /Library/Preferences/com.bigfix.BESAgent.plist
 sudo /usr/libexec/plistbuddy -c "Print :Settings:Client:$1:Value" /Library/Preferences/com.bigfix.BESAgent.plist
 
