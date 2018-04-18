dcdecd78dadc8ceaef9e56d8c3dd4baa87a8aeda
jgstew <james@jgstew.com>
2018-04-18 12:18:05 -0700

    Update bigfix_settings_read.sh
    It turns out that Plistbuddy doesn't do the right thing, particularly when a plist was recently changed.

--- bash/bigfix_settings_read.sh
+++ bash/bigfix_settings_read.sh
@@ -1,9 +1,7 @@
 
 # OS X
-# http://fgimian.github.io/blog/2015/06/27/a-simple-plistbuddy-tutorial/
-# http://blog.eexit.net/osx-dealing-with-property-list-files/
-# https://developer.apple.com/legacy/library/documentation/Darwin/Reference/ManPages/man8/PlistBuddy.8.html
+# https://macmule.com/2014/02/07/mavericks-preference-caching/
 # https://www.ibm.com/developerworks/community/wikis/home?lang=en#!/wiki/Tivoli+Endpoint+Manager/page/Common+File+Locations
-/usr/libexec/plistbuddy -c "Print :Settings:Client" /Library/Preferences/com.bigfix.BESAgent.plist
+defaults read /Library/Preferences/com.bigfix.BESAgent Settings
 
 # Linux/Unix ? TODO
