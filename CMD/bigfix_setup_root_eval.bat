deeef8e18c4f85eed929e752541e170f05b8d1b7
jgstew <james@jgstew.com>
2018-06-27 14:16:54 -0700

    Create bigfix_setup_root_eval.bat

---
+++ CMD/bigfix_setup_root_eval.bat
@@ -0,0 +1,7 @@
+
+REM Setup a non-production BigFix Eval Server with SQL Developer Edition.
+
+REM http://software.bigfix.com/download/bes/95/BigFix-BES-Server-9.5.9.62.exe
+REM http://download.microsoft.com/download/2/5/0/2508F7B4-6DDE-4C3E-B1FA-E1EB66F2F79F/SQLServer2016-SSEI-Dev.exe
+
+
