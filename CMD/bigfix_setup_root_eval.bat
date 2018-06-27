d314dae84603e2c5791776c49c820782179d76aa
jgstew <james@jgstew.com>
2018-06-27 14:19:11 -0700

    Update bigfix_setup_root_eval.bat

--- CMD/bigfix_setup_root_eval.bat
+++ CMD/bigfix_setup_root_eval.bat
@@ -1,7 +1,11 @@
 
+REM work in progress
 REM Setup a non-production BigFix Eval Server with SQL Developer Edition.
 
 REM http://software.bigfix.com/download/bes/95/BigFix-BES-Server-9.5.9.62.exe
 REM http://download.microsoft.com/download/2/5/0/2508F7B4-6DDE-4C3E-B1FA-E1EB66F2F79F/SQLServer2016-SSEI-Dev.exe
 
+SET BASEFOLDER=C:\Windows\Temp
+
+powershell -command "& { (New-Object Net.WebClient).DownloadFile('http://download.microsoft.com/download/2/5/0/2508F7B4-6DDE-4C3E-B1FA-E1EB66F2F79F/SQLServer2016-SSEI-Dev.exe', '%BASEFOLDER%\SQLServer-SSEI-Dev.exe') }" -ExecutionPolicy Bypass
 
