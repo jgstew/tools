d03ef2fd0cfeb6e4da8eaac93bc02a78512e2a53
jgstew <james@jgstew.com>
2018-06-28 19:24:40 -0700

    Create verifyAuthenticode.py

---
+++ Python/verifyAuthenticode.py
@@ -0,0 +1,14 @@
+
+# This is Windows Only
+# https://twitter.com/jgstew/status/1012509162540974080
+# https://programtalk.com/vs2/python/5137/PythonForWindows/windows/wintrust.py/
+# https://github.com/hakril/PythonForWindows/blob/master/samples/crypto/wintrust.py
+
+import windows.wintrust
+# git clone https://github.com/hakril/PythonForWindows.git
+# python .\PythonForWindows\setup.py install
+
+sFileName = r"C:\Windows\explorer.exe"
+
+print "signed: " + str( windows.wintrust.is_signed(sFileName) )
+print "0 is valid: " + str( windows.wintrust.check_signature(sFileName) )
