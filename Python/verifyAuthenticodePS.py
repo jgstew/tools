435a5931b4141decf5d992a1cd277862ce3023ca
jgstew <james@jgstew.com>
2018-06-29 13:40:58 -0700

    Update verifyAuthenticodePS.py

--- Python/verifyAuthenticodePS.py
+++ Python/verifyAuthenticodePS.py
@@ -8,11 +8,11 @@
 import subprocess
 import sys
 
-# TODO: using sFileName instead of hardcoded (parameterized)
 sFileName = r"C:\Windows\explorer.exe"
 # TODO: use `-ExecutionPolicy Bypass` somehow
 # https://stackoverflow.com/questions/21944895/running-powershell-script-within-python-script-how-to-make-python-print-the-pow
-psResult = subprocess.Popen( ["powershell", r'(Get-AuthenticodeSignature "C:\Windows\explorer.exe").Status -eq "Valid"'], stdout=sys.stdout )
+psResult = subprocess.Popen( ["powershell", r'(Get-AuthenticodeSignature "' + sFileName + r'").Status -eq "Valid"'], stdout=sys.stdout )
 psResult.communicate()
+
 
 # This will output `True` if the signature is valid.
