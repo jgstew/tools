19c0ce2b006235c36853db9cbde27292913df5ae
jgstew <james@jgstew.com>
2018-09-04 16:57:39 -0700

    Update docker_bigfix_client.sh

--- bash/docker_bigfix_client.sh
+++ bash/docker_bigfix_client.sh
@@ -16,9 +16,6 @@ fi
 sudo docker run -d --restart=unless-stopped ubuntu bash -c "apt-get update;apt-get install wget -y;wget https://raw.githubusercontent.com/jgstew/tools/master/bash/install_bigfix.sh;chmod u+x install_bigfix.sh;./install_bigfix.sh $RELAYFQDN;tail -f /dev/null"
 sudo docker run -d --restart=unless-stopped debian bash -c "apt-get update;apt-get install wget -y;wget https://raw.githubusercontent.com/jgstew/tools/master/bash/install_bigfix.sh;chmod u+x install_bigfix.sh;./install_bigfix.sh $RELAYFQDN;tail -f /dev/null"
 
-# can't seem to get opensuse to work with the default docker image. missing some dependancies.
-# sudo docker run -d opensuse bash -c "zypper --non-interactive install wget;wget https://raw.githubusercontent.com/jgstew/tools/master/bash/install_bigfix.sh;chmod u+x install_bigfix.sh;./install_bigfix.sh alpha.bigfix.com;tail -f /dev/null"
-
 sudo docker run -d --restart=unless-stopped centos bash -c "yum install initscripts -y;cd /tmp;curl -O https://raw.githubusercontent.com/jgstew/tools/master/bash/install_bigfix.sh;chmod u+x install_bigfix.sh;./install_bigfix.sh $RELAYFQDN;tail -f /dev/null"
 sudo docker run -d --restart=unless-stopped fedora bash -c "yum install initscripts -y;cd /tmp;curl -O https://raw.githubusercontent.com/jgstew/tools/master/bash/install_bigfix.sh;chmod u+x install_bigfix.sh;./install_bigfix.sh $RELAYFQDN;tail -f /dev/null"
 
@@ -26,6 +23,9 @@ sudo docker run -d --restart=unless-stopped oraclelinux bash -c "cd /tmp;curl -O
 
 # sudo docker run -d --restart=unless-stopped registry.access.redhat.com/rhel6 bash -c "yum install initscripts -y;cd /tmp;curl -O https://raw.githubusercontent.com/jgstew/tools/master/bash/install_bigfix.sh;chmod u+x install_bigfix.sh;./install_bigfix.sh $RELAYFQDN;tail -f /dev/null"
 # sudo docker run -d --restart=unless-stopped registry.access.redhat.com/rhel7 bash -c "yum install initscripts -y;cd /tmp;curl -O https://raw.githubusercontent.com/jgstew/tools/master/bash/install_bigfix.sh;chmod u+x install_bigfix.sh;./install_bigfix.sh $RELAYFQDN;tail -f /dev/null"
+
+# can't seem to get opensuse to work with the default docker image. missing some dependancies.
+# sudo docker run -d opensuse bash -c "zypper --non-interactive install wget;wget https://raw.githubusercontent.com/jgstew/tools/master/bash/install_bigfix.sh;chmod u+x install_bigfix.sh;./install_bigfix.sh alpha.bigfix.com;tail -f /dev/null"
 
 # TODO: https://hub.docker.com/_/amazonlinux/
 
