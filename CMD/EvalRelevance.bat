737e98f07c1c6e57e613dd7f5dfe2e9ec0b684f9
jgstew <james@jgstew.com>
2018-09-07 10:52:46 -0700

    Update EvalRelevance.bat

--- CMD/EvalRelevance.bat
+++ CMD/EvalRelevance.bat
@@ -5,6 +5,4 @@ Set Relevance=%*
 REM https://stackoverflow.com/a/23328830/861745
 for /f "tokens=2*" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\BigFix\EnterpriseClient" /v EnterpriseClientFolder 2^>^&1^|find "REG_"') do @set sPath=%%b
 
-echo %sPath%
-
 echo %Relevance% | "%sPath%qna" -t -showtypes
