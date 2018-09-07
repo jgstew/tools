267957f081213fc18f460bd8546bb8eca85251ad
jgstew <james@jgstew.com>
2018-09-07 11:10:20 -0700

    Update EvalRelevance.bat

--- CMD/EvalRelevance.bat
+++ CMD/EvalRelevance.bat
@@ -1,8 +1,13 @@
-REM Get QnA.exe location from registry, and pass parameters as relevance to evaluate (registry location only valid on x64 systems)
-@echo off
+REM @echo off
 Set Relevance=%*
 
+Set WoW=
+REM https://superuser.com/a/321996/413576
+REM https://stackoverflow.com/a/51502846/861745
+REM  if x64 OS, then read x32 registry
+if defined PROGRAMFILES(x86) Set WoW=\WOW6432Node
+
 REM https://stackoverflow.com/a/23328830/861745
-for /f "tokens=2*" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\BigFix\EnterpriseClient" /v EnterpriseClientFolder 2^>^&1^|find "REG_"') do @set sPath=%%b
+for /f "tokens=2*" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE%WoW%\BigFix\EnterpriseClient" /v EnterpriseClientFolder 2^>^&1^|find "REG_"') do @set sPath=%%b
 
 echo %Relevance% | "%sPath%qna" -t -showtypes
