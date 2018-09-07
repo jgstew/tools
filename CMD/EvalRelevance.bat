a00aac50d5f70c341205fe46901d1c2d937f0d90
jgstew <james@jgstew.com>
2018-09-07 10:52:24 -0700

    Update EvalRelevance.bat

--- CMD/EvalRelevance.bat
+++ CMD/EvalRelevance.bat
@@ -1,2 +1,10 @@
 
-echo %* | "C:\Program Files (x86)\BigFix Enterprise\BES Client\qna" -t -showtypes
+@echo off
+Set Relevance=%*
+
+REM https://stackoverflow.com/a/23328830/861745
+for /f "tokens=2*" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\BigFix\EnterpriseClient" /v EnterpriseClientFolder 2^>^&1^|find "REG_"') do @set sPath=%%b
+
+echo %sPath%
+
+echo %Relevance% | "%sPath%qna" -t -showtypes
