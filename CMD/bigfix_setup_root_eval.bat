951b5a628a1205e8e866664895ee2302647670c0
jgstew <james@jgstew.com>
2018-09-25 12:01:43 -0700

    Update bigfix_setup_root_eval.bat

--- CMD/bigfix_setup_root_eval.bat
+++ CMD/bigfix_setup_root_eval.bat
@@ -17,7 +17,7 @@ REM Download SQLServer Developer Edition:
 powershell -command "& { (New-Object Net.WebClient).DownloadFile('http://download.microsoft.com/download/2/5/0/2508F7B4-6DDE-4C3E-B1FA-E1EB66F2F79F/SQLServer2016-SSEI-Dev.exe', '%BASEFOLDER%\SQLServer-SSEI-Dev.exe') }" -ExecutionPolicy Bypass
 REM Install SQLServer Developer Edition:
 REM https://docs.microsoft.com/en-us/sql/database-engine/install-windows/install-sql-server-from-the-command-prompt?view=sql-server-2017
-%BASEFOLDER%\SQLServer-SSEI-Dev.exe
+%BASEFOLDER%\SQLServer-SSEI-Dev.exe /Q /ACTION=Install /IACCEPTSQLSERVERLICENSETERMS /MEDIAPATH="%BASEFOLDER%\SQLServerMedia"
 
 
 REM https://github.com/jgstew/bigfix-content/blob/f36708ebf7af38f84a286f3b79131faccb1d2e9c/fixlet/Install%20SQL%20Server%20Management%20Studio%2016.5.1%20-%20Windows.bes
