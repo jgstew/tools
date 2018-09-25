f5b77fa60aaa1566401e0e8c944184b2223cc6f9
jgstew <james@jgstew.com>
2018-09-25 11:29:07 -0700

    Update bigfix_setup_root_eval.bat

--- CMD/bigfix_setup_root_eval.bat
+++ CMD/bigfix_setup_root_eval.bat
@@ -5,7 +5,10 @@ REM Setup a non-production BigFix Eval Server with SQL Developer Edition.
 REM http://software.bigfix.com/download/bes/95/BigFix-BES-9.5.9.62.exe
 REM http://download.microsoft.com/download/2/5/0/2508F7B4-6DDE-4C3E-B1FA-E1EB66F2F79F/SQLServer2016-SSEI-Dev.exe
 
-SET BASEFOLDER=C:\Windows\Temp
+SET BASEFOLDER=C:\tmp
+
+REM Make folder if not existing:
+if not exist "%BASEFOLDER%" mkdir %BASEFOLDER%
 
 REM Open folder:
 start "" %BASEFOLDER%
