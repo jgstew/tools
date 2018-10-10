d6b7f649dc01b1ec470624ccf3df13795ffe1910
jgstew <james@jgstew.com>
2018-10-10 15:11:00 -0700

    Update import_cert_local.bat

--- CMD/import_cert_local.bat
+++ CMD/import_cert_local.bat
@@ -3,7 +3,7 @@ REM https://docs.microsoft.com/en-us/windows-server/administration/windows-comma
 
 certutil -f -p [password] -dump "cert.p12"
 
-REM TODO: validate expiration above
+REM get just expiration: https://github.com/jgstew/tools/blob/master/CMD/cert_file_expiration.bat
 
 certutil -f -p [password] -importpfx "cert.p12"
 
