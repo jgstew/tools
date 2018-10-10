ef4386574d1cec5392c239a6a6f9ecd34418829e
jgstew <james@jgstew.com>
2018-10-10 12:56:38 -0700

    Update import_cert_local.bat

--- CMD/import_cert_local.bat
+++ CMD/import_cert_local.bat
@@ -1,5 +1,9 @@
 
 REM https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/certutil#BKMK_importPFX
 
+certutil -f -p [password] -dump "cert.p12"
+
+REM TODO: validate expiration above
+
 certutil -f -p [password] -importpfx "cert.p12"
 
