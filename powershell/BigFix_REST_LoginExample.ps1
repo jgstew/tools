4bb7c363a4e4eb01e6681b1d4ce44563a74d4134
jgstew <james@jgstew.com>
2019-04-12 10:23:03 -0700

    Create BigFix_REST_LoginExample.ps1

---
+++ powershell/BigFix_REST_LoginExample.ps1
@@ -0,0 +1,30 @@
+# based upon script by Jeff Schafer
+
+add-type @"
+  using System.Net;
+  using System.Security.Cryptography.X509Certificates;
+  public class TrustAllCertsPolicy : ICertificatePolicy {
+      public bool CheckValidationResult(
+          ServicePoint srvPoint, X509Certificate certificate,
+          WebRequest request, int certificateProblem) {
+          return true;
+      }
+  }
+"@
+$AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
+[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+
+$server = 'bigfix' # BigFix Root Server address
+$port = '52311' # BigFix Root Server port
+$urlbase = "https://${server}:${port}/api/login" #May need to change port if not using 52311
+
+$credential = Get-Credential
+
+$EncodedPassword = [System.Convert]::ToBase64String( [System.Text.Encoding]::UTF8.GetBytes($credential.UserName + ':' + $credential.GetNetworkCredential().Password) )
+$headers = @{"Authorization"="Basic $($EncodedPassword)"}
+
+$url= $urlbase
+$response = Invoke-RestMethod -Uri $url -Method GET -Headers $headers
+
+Write-Host $response
