fcf602b0fb564cd39dbfbd17814ebcb2e967997d
jgstew <james@jgstew.com>
2019-04-12 10:54:13 -0700

    Update BigFix_REST_SessionRelevance.ps1

--- powershell/BigFix_REST_SessionRelevance.ps1
+++ powershell/BigFix_REST_SessionRelevance.ps1
@@ -40,5 +40,6 @@ $url= $urlbase + 'query?relevance=' + [uri]::EscapeDataString($SessionRelevanceQ
 
 #Write-Host $response.InnerXml #Get RAW XML
 
+# https://stackoverflow.com/a/6143482/861745
 # Get Answer(s): 
 Write-Host $response.BESAPI.Query.InnerXml
