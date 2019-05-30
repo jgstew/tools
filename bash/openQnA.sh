b546f7277e38f8381db6e09089878d93937a0b2d
jgstew <james@jgstew.com>
2019-05-30 16:58:28 -0700

    Update openQnA.sh

--- bash/openQnA.sh
+++ bash/openQnA.sh
@@ -3,6 +3,10 @@
 # https://www.ibm.com/developerworks/community/wikis/home?lang=en#!/wiki/Tivoli+Endpoint+Manager/page/Common+File+Locations
 QNAPATH="./qna"
 
+#if [ -f '/Program Files (x86)/BigFix Enterprise/BES Client' ]; then
+#  QNAPATH="/Program Files (x86)/BigFix Enterprise/BES Client/qna"
+#fi
+
 if [ -f /opt/BESClient/bin/qna ]; then
   QNAPATH="/opt/BESClient/bin/qna"
 fi
