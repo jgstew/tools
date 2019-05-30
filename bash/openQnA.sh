08d82672664ec4d27a6493ffff9a2c201dd81b97
jgstew <james@jgstew.com>
2019-05-30 16:00:34 -0700

    Update openQnA.sh

--- bash/openQnA.sh
+++ bash/openQnA.sh
@@ -3,10 +3,12 @@
 # https://www.ibm.com/developerworks/community/wikis/home?lang=en#!/wiki/Tivoli+Endpoint+Manager/page/Common+File+Locations
 QNAPATH="./qna"
 
+if [ -f /opt/BESClient/bin/qna ]; then
+  QNAPATH="/opt/BESClient/bin/qna"
+fi
+
 if [ -f /Library/BESAgent/BESAgent.app/Contents/MacOS/QnA ]; then
   QNAPATH="/Library/BESAgent/BESAgent.app/Contents/MacOS/QnA"
-else
-  QNAPATH="/opt/BESClient/bin/qna"
 fi
 
 $QNAPATH -showtypes
