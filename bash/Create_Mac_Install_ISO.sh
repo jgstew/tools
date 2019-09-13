de52642a20666f20585bac8a206a7dcbed200410
jgstew <james@jgstew.com>
2019-09-13 11:46:31 -0400

    Update Create_Mac_Install_ISO.sh

--- bash/Create_Mac_Install_ISO.sh
+++ bash/Create_Mac_Install_ISO.sh
@@ -4,5 +4,6 @@ cd /tmp
 
 bash -c "hdiutil attach /tmp/Catalina.dmg -noverify -nobrowse -mountpoint /Volumes/Catalina"
 
+# does this have a -nobrowse option? This appears on the desktop
 bash -c "sudo /Applications/Install\ macOS\ Catalina\ Beta.app/Contents/Resources/createinstallmedia --volume /Volumes/Catalina --nointeraction"
 
