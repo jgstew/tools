25c1a8e26193102967a73d07b82caabf597e73d5
jgstew <james@jgstew.com>
2019-09-13 11:54:17 -0400

    Update Create_Mac_Install_ISO.sh

--- bash/Create_Mac_Install_ISO.sh
+++ bash/Create_Mac_Install_ISO.sh
@@ -4,11 +4,26 @@
 
 cd /tmp
 
+# create a DMG to create the install media within
+bash -c "hdiutil create -o /tmp/Catalina -size 7900m -volname Catalina -layout SPUD -fs HFS+J"
+
+# mount it
 bash -c "hdiutil attach /tmp/Catalina.dmg -noverify -nobrowse -mountpoint /Volumes/Catalina"
 
+# createinstallmedia inside created volume
 # does this have a -nobrowse option? This appears on the desktop
 bash -c "sudo /Applications/Install\ macOS\ Catalina\ Beta.app/Contents/Resources/createinstallmedia --volume /Volumes/Catalina --nointeraction"
 
+# unmount/eject
 # get this path dynamically with relevance instead?
 bash -c "hdiutil detach /volumes/Install\ macOS\ Catalina\ Beta"
 
+# convert from DMG to ISO (cd format)
+bash -c "hdiutil convert /tmp/Catalina.dmg -format UDTO -o /tmp/Catalina.cdr"
+
+# rename
+bash -c "mv /tmp/Catalina.cdr /tmp/Catalina.iso"
+
+# ???
+# unmount/eject:  /Volumes/Catalina
+#        delete:  /tmp/Catalina.dmg
