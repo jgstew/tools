6bd3ecad2b7a8da14e218a0f751bc9e8e466947b
jgstew <james@jgstew.com>
2019-09-13 11:45:50 -0400

    Update Create_Mac_Install_ISO.sh

--- bash/Create_Mac_Install_ISO.sh
+++ bash/Create_Mac_Install_ISO.sh
@@ -3,3 +3,6 @@
 cd /tmp
 
 bash -c "hdiutil attach /tmp/Catalina.dmg -noverify -nobrowse -mountpoint /Volumes/Catalina"
+
+bash -c "sudo /Applications/Install\ macOS\ Catalina\ Beta.app/Contents/Resources/createinstallmedia --volume /Volumes/Catalina --nointeraction"
+
