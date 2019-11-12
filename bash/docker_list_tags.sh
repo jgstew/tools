cf39f0267c3f85036540da278aab836a18d8032b
jgstew <james@jgstew.com>
2019-11-12 10:31:49 -0800

    Create docker_list_tags.sh
    https://stackoverflow.com/questions/28320134/how-can-i-list-all-tags-for-a-docker-image-on-a-remote-registry

---
+++ bash/docker_list_tags.sh
@@ -0,0 +1,3 @@
+
+
+wget -q https://registry.hub.docker.com/v1/repositories/debian/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}' | grep -o "^[0-9]*" | uniq
