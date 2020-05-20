bf9f40543a3488359e659a3ac0fc4d44dddd00b3
jgstew <james@jgstew.com>
2020-05-20 14:55:17 -0400

    Update open_url_macos.sh

--- bash/open_url_macos.sh
+++ bash/open_url_macos.sh
@@ -1,2 +1,5 @@
 
 launchctl asuser `id -u` open "http://google.com"
+
+# BigFix Equivalent:
+# run launchctl asuser { unique value of ids of users of (current users; logged on users) } open "http://google.com"
