0f05eedac51632a79eae670f0060cf98ec5ffff6
jgstew <james@jgstew.com>
2020-06-02 16:57:05 -0400

    Update PS_VerifyFileSig.bat
    chktrust from mono project seems like it can verify signatures, but NOT of CAB files it seems.

--- CMD/PS_VerifyFileSig.bat
+++ CMD/PS_VerifyFileSig.bat
@@ -2,3 +2,5 @@
 powershell -ExecutionPolicy Bypass -command "(Get-AuthenticodeSignature \"C:\Temp\CatalogPC.cab\").Status -eq 'Valid'"
 
 REM https://stackoverflow.com/a/19876949
+REM https://linux.die.net/man/1/chktrust
+REM http://manpages.ubuntu.com/manpages/trusty/man1/chktrust.1.html
