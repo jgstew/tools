503dffc90eced4f070831dde43250e1e1c32b747
jgstew <james@jgstew.com>
2020-09-02 11:44:35 -0400

    new sed example

---
+++ bash/sed_ini_edit_value.sh
@@ -0,0 +1,10 @@
+
+# WARNING: `-i ''` causes this to directly overwrite the existing file with changes
+sed -i '' '/\(^use_fully_qualified_names *= *\).*/ s//\1False/' sssd.conf
+
+
+# References:
+# - https://stackoverflow.com/questions/7573368/in-place-edits-with-sed-on-os-x
+# - https://stackoverflow.com/questions/20568515/how-to-use-sed-to-replace-a-config-files-variable
+# - https://stackoverflow.com/questions/19567275/modifying-ini-files-using-shell-script
+# - https://stackoverflow.com/questions/2464760/modify-config-file-using-bash-script
