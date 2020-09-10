ae9ebfba3d91590484195f16a2c121ecdde89b7b
jgstew <james@jgstew.com>
2020-09-10 14:30:34 -0400

    Update ReadFile_NewUsers.ps1

--- powershell/ReadFile_NewUsers.ps1
+++ powershell/ReadFile_NewUsers.ps1
@@ -1,7 +1,7 @@
 
 Start-Transcript ($MyInvocation.MyCommand.Source + ".log") # -Append
 
-# match `first.last` within ` first.last@demo.com `
+# match `first.last` within ` first.last@demo.com ` or within ` first.last `
 #  - regexr.com/5bqku
 $REGEX = " *(\w+\.\w+)[@ \n]"
 
@@ -11,11 +11,16 @@ foreach( $line in Get-Content ($MyInvocation.MyCommand.Source + ".txt") ) {
         # Get the first RegEx Match, Get first capture Group value
         # - https://stackoverflow.com/questions/33913878/how-to-get-the-captured-groups-from-select-string
         $parsed = [regex]::Match($line, $REGEX)[0].Groups[1].Value
-        Write-Host $parsed
+        Write-Host $parsed # first.last
     }
 }
 
 Stop-Transcript
 
 # References:
-#  - https://stackoverflow.com/questions/33511772/read-file-line-by-line-in-powershell
+# - https://stackoverflow.com/questions/33511772/read-file-line-by-line-in-powershell
+# - https://stackoverflow.com/questions/33913878/how-to-get-the-captured-groups-from-select-string 
+
+# Related:
+# - https://github.com/jgstew/tools/blob/master/powershell/ad_New-ADUser.ps1
+# - https://github.com/jgstew/tools/tree/master/powershell
