2c8e7cb1fdc010309804d1702586a91ad7403734
jgstew <james@jgstew.com>
2021-05-14 11:40:29 -0400

    Update install_bigfix.sh

--- bash/install_bigfix.sh
+++ bash/install_bigfix.sh
@@ -47,7 +47,7 @@ fi
 # these variables are typically set to the latest version of the BigFix agent
 # URLMAJORMINOR is the first two integers of URLVERSION
 #  most recent version# found here under `Agent`:  http://support.bigfix.com/bes/release/
-URLVERSION=10.0.1.41
+URLVERSION=10.0.3.66
 URLMAJORMINOR=`echo $URLVERSION | awk '/./ {gsub(/\./, " "); print $1 $2}'`
 
 # check for x32bit or x64bit OS
