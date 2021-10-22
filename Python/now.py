977361bc1410ebda7199ab3ca2020b53d24d270c
jgstew <james@jgstew.com>
2021-10-22 16:57:46 -0400

    slight update

--- Python/now.py
+++ Python/now.py
@@ -1,2 +1,7 @@
+"""
+Return the current time in the same format is BigFix Relevance.
+"""
 import datetime
-print(datetime.datetime.now(datetime.timezone.utc).strftime('%a, %d %b %Y %H:%M:%S %z'))
+
+time_now = datetime.datetime.now(datetime.timezone.utc)
+print(time_now.strftime("%a, %d %b %Y %H:%M:%S %z"))
