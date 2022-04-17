6d9507fd23453f125370fa2e057a9cc10f9f8da6
jgstew <james@jgstew.com>
2022-04-17 13:54:13 -0400

    add comment about running on windows

--- swift/Predicate.swift
+++ swift/Predicate.swift
@@ -1,5 +1,8 @@
 #!/usr/bin/env swift
 
+// To run on windows:
+// - C:\Library\Developer\Toolchains\unknown-Asserts-development.xctoolchain\usr\bin\swift.exe .\Predicate.swift -sdk C:\Library\Developer\Platforms\Windows.platform\Developer\SDKs\Windows.sdk -I C:\Library\Developer\Platforms\Windows.platform\Developer\SDKs\Windows.sdk\usr\lib\swift -L C:\Library\Developer\Platforms\Windows.platform\Developer\SDKs\Windows.sdk\usr\lib\swift\windows
+// Current get ERROR: `unable to load standard library for target 'x86_64-unknown-windows-msvc'`
 import Foundation
 
 // https://www.swiftbysundell.com/articles/predicates-in-swift/
