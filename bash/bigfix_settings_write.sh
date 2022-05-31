28f406838960419a88d9e222a95af5eb334707c3
JGStew <james@jgstew.com>
2022-05-31 14:20:46 -0400

    fix settings write to work even if setting does not already exist

--- bash/bigfix_settings_write.sh
+++ bash/bigfix_settings_write.sh
@@ -3,10 +3,11 @@
 #    - https://gist.github.com/gregneagle/01c99322cf985e771827
 # PlistBuddy does not work correctly on MacOS due to CFPrefs caching without stopping the agent first
 
-echo stopping the bigfix client so the edit will work.
+echo stopping the bigfix client so the edit will work:
 
 sudo /Library/BESAgent/BESAgent.app/Contents/MacOS/BESAgentControlPanel.sh -stop
 sudo /Library/BESAgent/BESAgent.app/Contents/MacOS/BESAgentControlPanel.sh -status
+echo ""
 
 # Make sure values are provided:
 if [ -n "$1" ]; then
@@ -24,9 +25,10 @@ fi
 # OS X
 # NOTE: the following is only valid if working with plain files not in use by BigFix/CFPrefs
 #        see here: https://macmule.com/2014/02/07/mavericks-preference-caching/
-sudo /usr/libexec/plistbuddy -c "Set :Settings:Client:$1:Value $2" /Library/Preferences/com.bigfix.BESAgent.plist
+sudo /usr/libexec/plistbuddy -c "Add :Settings:Client:$1:Value string $2" /Library/Preferences/com.bigfix.BESAgent.plist
 sudo /usr/libexec/plistbuddy -c "Print :Settings:Client:$1:Value" /Library/Preferences/com.bigfix.BESAgent.plist
 
 echo starting bigfix:
 sudo /Library/BESAgent/BESAgent.app/Contents/MacOS/BESAgentControlPanel.sh -start
 sudo /Library/BESAgent/BESAgent.app/Contents/MacOS/BESAgentControlPanel.sh -status
+echo ""
