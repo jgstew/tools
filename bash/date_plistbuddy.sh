d5a994024cb45ba0eafec61721b6e92a0ed4ad67
JGStew <james@jgstew.com>
2022-05-31 15:14:59 -0400

    added comment for date formatting

--- bash/date_plistbuddy.sh
+++ bash/date_plistbuddy.sh
@@ -1,5 +1,6 @@
 
 # Related: https://github.com/jgstew/tools/blob/master/bash/now.sh
 
+# https://www.cyberciti.biz/faq/linux-unix-formatting-dates-for-display/
 # example: "Sat Jun 27 18:51:00 EST 2022"
 date +"%a %b %d %T %Z %Y"
