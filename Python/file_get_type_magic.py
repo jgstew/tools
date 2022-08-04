60445991ddadfce4639630f3f674ccdbbb8d8767
jgstew <james@jgstew.com>
2022-08-04 14:35:29 -0400

    add example for python magic get file type, add new items to pefile get version from exe

---
+++ Python/file_get_type_magic.py
@@ -0,0 +1,6 @@
+# pip install python-magic
+# pip install python-magic-bin
+
+import magic
+
+print(magic.from_file(r"C:\Windows\explorer.exe"))
--- Python/file_get_version_exe_pefile.py
+++ Python/file_get_version_exe_pefile.py
@@ -13,30 +13,37 @@ From:
 
 import pefile
 
+
 def dump_info_pefile(filepath):
     pe = pefile.PE(filepath)
 
     pe_info_dict = {}
 
-    if hasattr(pe, 'VS_VERSIONINFO'):
+    if hasattr(pe, "VS_VERSIONINFO"):
         for idx in range(len(pe.VS_VERSIONINFO)):
-            if hasattr(pe, 'FileInfo') and len(pe.FileInfo) > idx:
+            if hasattr(pe, "FileInfo") and len(pe.FileInfo) > idx:
                 for entry in pe.FileInfo[idx]:
-                    if hasattr(entry, 'StringTable'):
+                    if hasattr(entry, "StringTable"):
                         for st_entry in entry.StringTable:
                             for str_entry in sorted(list(st_entry.entries.items())):
-                                pe_info_dict[str_entry[0].decode('utf-8', 'backslashreplace')] = str_entry[1].decode('utf-8', 'backslashreplace')
+                                pe_info_dict[
+                                    str_entry[0].decode("utf-8", "backslashreplace")
+                                ] = str_entry[1].decode("utf-8", "backslashreplace")
                                 # print('{0}: {1}'.format(
                                 #     str_entry[0].decode('utf-8', 'backslashreplace'),
                                 #     str_entry[1].decode('utf-8', 'backslashreplace')))
     return pe_info_dict
+
 
 def main():
     filepath = r"C:\Program Files\7-Zip\7z.exe"
     pe_info_dict = dump_info_pefile(filepath)
 
     print(pe_info_dict)
-    
+    print(pe_info_dict["FileVersion"])
+    print(pe_info_dict["ProductVersion"])
+    print(pe_info_dict["ProductName"])
+
 
 if __name__ == "__main__":
     main()
