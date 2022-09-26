1a43fb18182bd6c813c9575cad942643b9c915f2
jgstew <james@jgstew.com>
2022-09-26 12:14:55 -0400

    add code that enumerates all signatures

--- Python/get_pefile_signify_time.py
+++ Python/get_pefile_signify_time.py
@@ -7,7 +7,22 @@ Related:
 from signify.authenticode.signed_pe import SignedPEFile
 
 pathname = r"C:\Windows\explorer.exe"
+# pathname = r"VirtualBox-Win.exe"
 
 with open(pathname, "rb") as f:
     pefile = SignedPEFile(f)
+
+    print("first signature timestamp:")
     print(list(pefile.signed_datas)[0].signer_infos[0].countersigner.signing_time)
+
+    sig_times = []
+
+    print("all signature times: (most files only have 1)")
+    for datas in list(pefile.signed_datas):
+        for infos in datas.signer_infos:
+            sig_time = infos.countersigner.signing_time
+            sig_times.append(sig_time)
+            print(sig_time)
+
+    print("maximum signature timestamp:")
+    print(max(sig_times))
