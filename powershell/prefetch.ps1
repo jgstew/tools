e4f020da7ec8aab43dee6360f63562602545e3d1
jgstew <james@jgstew.com>
2022-11-01 12:47:50 -0400

    fix usage comment

--- powershell/prefetch.ps1
+++ powershell/prefetch.ps1
@@ -1,12 +1,15 @@
-# powershell -ExecutionPolicy Bypass .\prefetch.ps1 "prefetch string.txt sha1:? size:? url sha256:?"
-# powershell -ExecutionPolicy Bypass .\prefetch.ps1 "add prefetch item name=string.txt sha1=? size=? url=? sha256=?"
+# Usage Examples:
+# prefetch statement:
+# powershell -ExecutionPolicy Bypass .\prefetch.ps1 "'prefetch string.txt sha1:? size:? url sha256:?'"
+# prefetch block item:
+# powershell -ExecutionPolicy Bypass .\prefetch.ps1 "'add prefetch item name=string.txt sha1=? size=? url=? sha256=?'"
 $prefetch = $args[0]
 
 if (!$prefetch) { 
     $prefetch = "prefetch unzip.exe sha1:84debf12767785cd9b43811022407de7413beb6f size:204800 http://software.bigfix.com/download/redist/unzip-6.0.exe sha256:2122557d350fd1c59fb0ef32125330bde673e9331eb9371b454c2ad2d82091ac"
 }
 
-# Write-Host $prefetch
+Write-Host $prefetch
 
 $prefetch_name = ( select-string "(?:name=|prefetch )([a-zA-Z0-9\.]+)(?: |$)" -inputobject $prefetch ).Matches.groups[1].value
 
