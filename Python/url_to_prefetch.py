e81cc507b3d6e0db04bbef4700eb5457dcc351dc
jgstew <james@jgstew.com>
2022-11-07 15:05:22 -0500

    tweaks and improvements for url_to_prefetch

--- Python/url_to_prefetch.py
+++ Python/url_to_prefetch.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python
 # moved to https://github.com/jgstew/bigfix_prefetch/blob/master/src/bigfix_prefetch/prefetch_from_url.py
 
+import sys
+
 try:
-  import bigfix_prefetch
+    import bigfix_prefetch
 except ModuleNotFoundError:
-  print("")
-  print("ERROR: requires `bigfix_prefetch` - install with:")
-  print("    pip install bigfix-prefetch")
-  print(" -------------------------------- ")
-  raise
+    print("")
+    print("ERROR: requires `bigfix_prefetch` - install with:")
+    print("    pip install bigfix-prefetch")
+    print(" -------------------------------- ")
+    raise
 
 
 # if called directly, then run this example:
-if __name__ == '__main__':
-  print(bigfix_prefetch.prefetch_from_url("google.com"))
+if __name__ == "__main__":
+    # get first argument as url:
+    try:
+        url = sys.argv[1]
+    except IndexError:
+        url = None
+
+    # if no url, use a default for demo purposes:
+    if not url:
+        url = "http://software.bigfix.com/download/redist/unzip-6.0.exe"
+
+    if "http" not in url or "://" not in url:
+        print("ERROR: invalid URL: ", url)
+        sys.exit(1)
+
+    # get prefetch and print to console:
+    print(bigfix_prefetch.prefetch_from_url.url_to_prefetch(url))
