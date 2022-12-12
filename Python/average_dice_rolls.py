d3af3deb9c9f9f6dbe9e7738f10a76b671120381
jgstew <james@jgstew.com>
2022-12-11 20:57:20 -0500

    get all

--- Python/average_dice_rolls.py
+++ Python/average_dice_rolls.py
@@ -1,13 +1,23 @@
 import random
 
 
+def dice_roll_text(num_sides=6, num_dice=1, modifier=0):
+    mod_text = ""
+    if modifier != 0:
+        if modifier > 0:
+            mod_text = "+" + str(modifier)
+        else:
+            mod_text = str(modifier)
+    return str(num_dice) + "d" + str(num_sides) + mod_text
+
+
 def average_dice_rolls(num_sides=6, num_dice=1, modifier=0, rolls=1000):
     total = 0
     for _ in range(rolls):
         roll = sum(random.randint(1, num_sides) for _ in range(num_dice))
         total += roll + modifier
 
-    return total / rolls
+    return round(total / rolls, 2)
 
 
 def min_dice_roll(num_dice=1, modifier=0):
@@ -20,15 +30,23 @@ def max_dice_roll(num_sides=6, num_dice=1, modifier=0):
 
 def main():
     print("main()")
-    print(average_dice_rolls(6, 1))
-    print(average_dice_rolls(6, 2))
-    print(average_dice_rolls(6, 3))
-    print(average_dice_rolls(8, 1))
-    print(average_dice_rolls(8, 2))
-    print(average_dice_rolls(8, 3))
-    print(average_dice_rolls(10, 1))
-    print(average_dice_rolls(10, 2))
-    print(average_dice_rolls(10, 3))
+
+    die = [1, 2, 3]
+    sides = [4, 6, 8, 10, 20]
+    modifiers = [-1, 0, 1, 2]
+
+    for dice in die:
+        for side in sides:
+            for mod in modifiers:
+                print(
+                    dice_roll_text(side, dice, mod)
+                    + " min:"
+                    + str(min_dice_roll(dice, mod))
+                    + " avg:"
+                    + str(average_dice_rolls(side, dice, mod))
+                    + " max:"
+                    + str(max_dice_roll(side, dice, mod))
+                )
 
 
 if __name__ == "__main__":
