b150de558da59d3ecaf484285eb1cc313a4c49f7
jgstew <james@jgstew.com>
2023-02-01 10:43:11 -0500

    fix for python 3

--- Python/yyyymmddhhmmss.py
+++ Python/yyyymmddhhmmss.py
@@ -1,13 +1,14 @@
-
 import time
 
 # http://stackoverflow.com/questions/2487109/python-date-time-formatting
 # https://docs.python.org/2/library/datetime.html
-def yyyymmddhhmmss(before="",after="",now=time.localtime()):
-  return before + time.strftime("%Y%m%d%H%M%S",now) + after
+def yyyymmddhhmmss(before="", after="", now=time.localtime()):
+    return before + time.strftime("%Y%m%d%H%M%S", now) + after
 
-def yyyymmddhhmm(before="",after="",now=time.localtime()):
-  return before + time.strftime("%Y%m%d%H%M",now) + after
+
+def yyyymmddhhmm(before="", after="", now=time.localtime()):
+    return before + time.strftime("%Y%m%d%H%M", now) + after
+
 
 if __name__ == "__main__":
-  print yyyymmddhhmmss()
+    print(yyyymmddhhmmss())
