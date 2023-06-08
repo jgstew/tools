5aa8faf33f029aa48e263df0d4402307ab3a9b92
JGStew <james@jgstew.com>
2023-06-08 16:39:29 -0400

    Update bigfix_run_qna_debian.sh
    add comment

--- bash/bigfix_run_qna_debian.sh
+++ bash/bigfix_run_qna_debian.sh
@@ -2,6 +2,7 @@ if ! [ -f /tmp/BESAgent-debian.deb ]; then
 curl -o /tmp/BESAgent-debian.deb https://software.bigfix.com/download/bes/100/BESAgent-10.0.9.21-debian6.amd64.deb
 fi
 
+# https://www.cyberciti.biz/faq/how-to-extract-a-deb-file-without-opening-it-on-debian-or-ubuntu-linux/
 ar x /tmp/BESAgent-debian.deb --output=/tmp
 
 tar --overwrite --extract --file=/tmp/data.tar.gz --directory=/tmp --exclude=var* --exclude=etc* --exclude=lib*
