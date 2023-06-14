1a7914422974ee189b0c07dfbfd22f5b14daecfd
jgstew <james@jgstew.com>
2023-06-14 13:17:25 -0400

    working cmd script to run bigfix QNA

---
+++ CMD/bigfix_run_qna_win.bat
@@ -0,0 +1,13 @@
+
+@ECHO OFF
+if not exist "\Windows\Temp\bigfix_qna" (
+    mkdir "\Windows\Temp\bigfix_qna"
+)
+
+if not exist "\Windows\Temp\bigfix_qna\QNA.zip" (
+    curl -o "\Windows\Temp\bigfix_qna\QNA.zip" https://software.bigfix.com/download/bes/100/util/QNA10.0.7.52.zip
+)
+
+tar -xf "\Windows\Temp\bigfix_qna\QNA.zip" -C "\Windows\Temp\bigfix_qna"
+
+"\Windows\Temp\bigfix_qna\QNA.exe" -showtypes
