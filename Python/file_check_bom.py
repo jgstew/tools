775e870b64197a2284f055892dfb32b2b547073c
jgstew <james@jgstew.com>
2023-07-10 11:41:16 -0400

    python example to check for utf8 with bom

---
+++ Python/file_check_bom.py
@@ -0,0 +1,14 @@
+﻿import codecs
+
+file_path = "Python/file_check_bom.py"
+
+encoding = "Unknown"
+
+required_bom = getattr(codecs, "BOM_UTF8")
+
+with open(file_path, "rb") as file:
+    header = file.read(len(required_bom))
+    if header.startswith(required_bom):
+        encoding = "utf-8-bom"
+
+print(encoding)
