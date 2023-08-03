eb86d8aac84448f16ab61d6e1398cb10d41f8c07
jgstew <james@jgstew.com>
2023-08-03 16:10:13 -0400

    example file time

--- Python/file_get_stat_time.py
+++ Python/file_get_stat_time.py
@@ -1,3 +1,17 @@
 import os
 
-print(os.stat(os.path.abspath(__file__)))
+file_stat_result = os.stat(os.path.abspath(__file__))
+
+# print(file_stat_result)
+
+file_stat_size = file_stat_result.st_size
+
+print(f"file size: {file_stat_size}")
+
+file_stat_times = [
+    file_stat_result.st_atime,
+    file_stat_result.st_ctime,
+    file_stat_result.st_mtime,
+]
+
+print(file_stat_times)
