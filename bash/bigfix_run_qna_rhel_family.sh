deebf6d26dff891526733e32c223606d1776631f
jgstew <james@jgstew.com>
2024-01-11 14:40:44 -0500

    add rhel family qna

---
+++ bash/bigfix_run_qna_rhel_family.sh
@@ -0,0 +1,25 @@
+
+args=$*
+
+if ! [ -f /tmp/qna ]; then
+mkdir /tmp/qna
+fi
+
+if ! [ -f /tmp/qna/BESAgent-rhe6.x86_64.rpm ]; then
+curl -o /tmp/qna/BESAgent-rhe6.x86_64.rpm https://software.bigfix.com/download/bes/100/BESAgent-10.0.10.46-rhe6.x86_64.rpm
+fi
+
+cd /tmp/qna
+
+# https://stackoverflow.com/questions/18787375/how-do-i-extract-the-contents-of-an-rpm
+rpm2cpio /tmp/qna/BESAgent-rhe6.x86_64.rpm | cpio -imvud
+
+# rm --dir /tmp/lib
+
+# get arg length
+len=${#args}
+if [ $len -lt 3 ]; then
+/tmp/qna/opt/BESClient/bin/qna -showtypes
+else
+echo $args | /tmp/qna/opt/BESClient/bin/qna -showtypes
+fi
