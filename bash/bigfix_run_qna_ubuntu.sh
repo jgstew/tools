71b632dc73abceb8d55b9216f07983a1ad687a4a
JGStew <james@jgstew.com>
2024-06-19 00:22:37 -0400

    switch ubuntu qna script

---
+++ bash/bigfix_run_qna_ubuntu.sh
@@ -0,0 +1,27 @@
+
+args=$*
+
+# TODO: detect if Ubuntu
+# cat /etc/os-release /etc/lsb-release | grep --ignore-case --max-count=1 --count ubuntu
+
+if ! [ -f /tmp/BESAgent-ubuntu.deb ]; then
+curl -o /tmp/BESAgent-ubuntu.deb https://software.bigfix.com/download/bes/110/BESAgent-11.0.2.125-ubuntu18.amd64.deb
+fi
+
+# https://www.cyberciti.biz/faq/how-to-extract-a-deb-file-without-opening-it-on-debian-or-ubuntu-linux/
+ar x /tmp/BESAgent-ubuntu.deb --output=/tmp
+
+tar --overwrite --extract --file=/tmp/data.tar.gz --directory=/tmp --exclude=var* --exclude=etc* --exclude=lib/*
+
+rm /tmp/control.tar.gz
+rm /tmp/data.tar.gz
+rm /tmp/debian-binary
+rm --dir /tmp/lib
+
+# get arg length
+len=${#args}
+if [ $len -lt 3 ]; then
+/tmp/opt/BESClient/bin/qna -showtypes
+else
+echo $args | /tmp/opt/BESClient/bin/qna -showtypes
+fi
