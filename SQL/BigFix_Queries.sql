2cfe9b18c6589275105a35b1368fe8c7b1c0693c
JGStew <james@jgstew.com>
2025-10-24 18:20:02 -0400

    Refactor QueryRelevance extraction in BigFix queries
    Replaced XML extraction with custom function for QueryRelevance and added Targeting field extraction.

--- SQL/BigFix_Queries.sql
+++ SQL/BigFix_Queries.sql
@@ -2,8 +2,10 @@
 SELECT [QueryID]
       ,[Operator]
       ,[CreationTime]
-      ,CAST(CAST([Fields] AS VARBINARY(MAX)) AS XML).value(
-        '(/Object/Fields/Contents/text())[5]', -- XPath to the node
-        'NVARCHAR(MAX)' -- SQL data type to return
-      ) AS QueryRelevance
+	    ,dbo.fn_ExtractField('Query', 1, Fields) as 'QueryRelevance'
+	    ,dbo.fn_ExtractField('Targeting', 0, Fields) as 'Targeting'
+      --,CAST(CAST([Fields] AS VARBINARY(MAX)) AS XML).value(
+      --  '(/Object/Fields/Contents/text())[5]', -- XPath to the node
+      --  'NVARCHAR(MAX)' -- SQL data type to return
+      --) AS 'QueryRelevance2'
   FROM [BFEnterprise].[dbo].[QUERIES]
