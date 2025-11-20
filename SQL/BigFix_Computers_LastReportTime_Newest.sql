76ff29cddcb5b7db045093c61c1fd10c11a82035
JGStew <james@jgstew.com>
2025-11-20 16:05:28 -0500

    Get newest computer last report time from DB
    Retrieve the most recent last report time from the COMPUTERS table.

--- SQL/BigFix_Computers_LastReportTime_Newest.sql
+++ SQL/BigFix_Computers_LastReportTime_Newest.sql
@@ -1,5 +1,12 @@
+/****** Get newest computer last report time in DB ******/
+SELECT TOP (1) 
+       [LastReportTime]
+  FROM [BFEnterprise].[dbo].[COMPUTERS]
+  ORDER BY [LastReportTime] DESC
+/******
 SELECT TOP 1 [Value]
   FROM [BFEnterprise].[dbo].[BES_COLUMN_HEADINGS]
  WHERE [Name] = 'Last Report Time'
 -- Convert the text to a date, then sort Descending (Newest first)
 ORDER BY CAST(SUBSTRING([Value], 6, 20) AS DATETIME) DESC
+******/
