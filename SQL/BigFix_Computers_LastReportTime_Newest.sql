9ea90e87812be8ae4fb5253e037122ee30c5239f
JGStew <james@jgstew.com>
2025-11-20 17:45:09 -0500

    Add query for maximum LastReportTime
    Added a query to get the maximum LastReportTime from the COMPUTERS table.

--- SQL/BigFix_Computers_LastReportTime_Newest.sql
+++ SQL/BigFix_Computers_LastReportTime_Newest.sql
@@ -1,8 +1,12 @@
 /****** Get newest computer last report time in DB ******/
+SELECT MAX(LastReportTime) AS LastReportTimeMax 
+  FROM [BFEnterprise].[dbo].[COMPUTERS]
+/****** 
 SELECT TOP (1) 
        [LastReportTime]
   FROM [BFEnterprise].[dbo].[COMPUTERS]
   ORDER BY [LastReportTime] DESC
+******/
 /******
 SELECT TOP 1 [Value]
   FROM [BFEnterprise].[dbo].[BES_COLUMN_HEADINGS]
