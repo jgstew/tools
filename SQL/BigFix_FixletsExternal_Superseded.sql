102c93f0ec385a412874c0c33b0dc41f1b4b8fc6
JGStew <james@jgstew.com>
2026-02-10 18:33:20 -0500

    Refactor SQL query for superseded fixlets

--- SQL/BigFix_FixletsExternal_Superseded.sql
+++ SQL/BigFix_FixletsExternal_Superseded.sql
@@ -1,9 +1,9 @@
 /****** SideID, FixletID of Superseded Fixlets ******/
 SELECT 
-      S.[SiteID]
-    , E.[ID] AS [FixletID]
-FROM [BFEnterprise].[dbo].[EXTERNAL_OBJECT_DEFS] AS E
-INNER JOIN [BFEnterprise].[dbo].[SITENAMEMAP] AS S
-    ON E.[Sitename] = S.[Sitename]
-WHERE E.[IsFixlet] = 1
-  AND E.[Name] LIKE '% (Superseded)%'
+      SiteNameMapTable.[SiteID]
+    , ExternalFixletsTable.[ID] AS [FixletID]
+FROM [BFEnterprise].[dbo].[EXTERNAL_OBJECT_DEFS] AS ExternalFixletsTable
+INNER JOIN [BFEnterprise].[dbo].[SITENAMEMAP] AS SiteNameMapTable
+    ON ExternalFixletsTable.[Sitename] = SiteNameMapTable.[Sitename]
+WHERE ExternalFixletsTable.[IsFixlet] = 1
+  AND ExternalFixletsTable.[Name] LIKE '% (Superseded)%'
