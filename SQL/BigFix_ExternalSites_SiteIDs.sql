50efe03bf76cb9a68e1e5822e3200144721e5c0f
JGStew <james@jgstew.com>
2026-07-27 13:38:04 -0400

    Add SQL query for External SiteID mapping
    Add SQL query for External SiteID mapping

---
+++ SQL/BigFix_ExternalSites_SiteIDs.sql
@@ -0,0 +1,6 @@
+/****** Mapping of External SiteID to Sitename ******/
+SELECT [SiteID]
+      ,[UndecoratedSitename]
+      ,[SiteURL]
+      ,[ModificationTime]
+  FROM [BFEnterprise].[dbo].[SITENAMEMAP]
