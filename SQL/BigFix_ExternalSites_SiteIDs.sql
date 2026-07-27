6e2467583fe90d4dc1750616016fdafe59d18cd5
JGStew <james@jgstew.com>
2026-07-27 13:55:55 -0400

    Format SQL query in BigFix_ExternalSites_SiteIDs.sql for improved readability

--- SQL/BigFix_ExternalSites_SiteIDs.sql
+++ SQL/BigFix_ExternalSites_SiteIDs.sql
@@ -1,10 +1,11 @@
 /****** Mapping of External SiteID to Sitename
+
 Related:
 - https://github.com/jgstew/tools/blob/master/bash/bfsite_get_id.sh
 - https://sync.bigfix.com/bfsites/bessupport_1513/SiteMap.js
  ******/
 SELECT [SiteID]
-      ,[UndecoratedSitename]
-      ,[SiteURL]
-      ,[ModificationTime]
-  FROM [BFEnterprise].[dbo].[SITENAMEMAP]
+      , [UndecoratedSitename]
+      , [SiteURL]
+      , [ModificationTime]
+FROM [BFEnterprise].[dbo].[SITENAMEMAP]
