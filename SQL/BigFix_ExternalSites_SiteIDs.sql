d82c6c480ddd78086f1637f9f8e8c7d651f6753c
JGStew <james@jgstew.com>
2026-07-27 13:50:07 -0400

    Update comments in BigFix_ExternalSites_SiteIDs.sql to format related links

--- SQL/BigFix_ExternalSites_SiteIDs.sql
+++ SQL/BigFix_ExternalSites_SiteIDs.sql
@@ -1,5 +1,7 @@
 /****** Mapping of External SiteID to Sitename
-Related: https://github.com/jgstew/tools/blob/master/bash/bfsite_get_id.sh
+Related:
+- https://github.com/jgstew/tools/blob/master/bash/bfsite_get_id.sh
+- https://sync.bigfix.com/bfsites/bessupport_1513/SiteMap.js
  ******/
 SELECT [SiteID]
       ,[UndecoratedSitename]
