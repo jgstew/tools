34cf20a3349eb4c91140b3e750df274fb9ebf868
JGStew <james@jgstew.com>
2026-07-27 14:40:25 -0400

    Update comments in BigFix_ExternalSites_SiteIDs.sql for clarity and context

--- SQL/BigFix_ExternalSites_SiteIDs.sql
+++ SQL/BigFix_ExternalSites_SiteIDs.sql
@@ -1,5 +1,8 @@
 /****** Mapping of External SiteID to Sitename
 
+This table is populated by the BESAdmin tool.
+If you have not run BESAdmin recently then it may be out of date.
+
 Related:
 - https://github.com/jgstew/tools/blob/master/bash/bfsite_get_id.sh
 - https://sync.bigfix.com/bfsites/bessupport_1513/SiteMap.js
